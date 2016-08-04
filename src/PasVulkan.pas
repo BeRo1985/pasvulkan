d572d26bb5395c370ed03c67de1755fd966cdfdc
Benjamin Rosseaux <benjamin@rosseaux.de>
2016-08-04 09:56:11 +0200

    - More work on PasVulkan.pas

--- src/PasVulkan.pas
+++ src/PasVulkan.pas
@@ -1,7 +1,7 @@
 (******************************************************************************
  *                                 PasVulkan                                  *
  ******************************************************************************
- *                        Version 2016-08-04-09-47-0000                       *
+ *                        Version 2016-08-04-09-55-0000                       *
  ******************************************************************************
  *                                zlib license                                *
  *============================================================================*
@@ -32,21 +32,23 @@
  *    license.                                                                *
  * 2. The zlib license header goes at the top of each source file, with       *
  *    appropriate copyright notice.                                           *
- * 3. After a pull request, check the status of your pull request on          *
+ * 3. This PasVulkan wrapper may be used only with the PasVulkan-own Vulkan   *
+ *    Pascal header.                                                          *
+ * 4. After a pull request, check the status of your pull request on          *
       http://github.com/BeRo1985/pasvulkan                                    *
- * 4. Write code, which is compatible with Delphi 7-XE7 and FreePascal >= 3.0 *
+ * 5. Write code, which is compatible with Delphi 7-XE7 and FreePascal >= 3.0 *
  *    so don't use generics/templates, operator overloading and another newer *
  *    syntax features than Delphi 7 has support for that, but if needed, make *
  *    it out-ifdef-able.                                                      *
- * 5. Don't use Delphi-only, FreePascal-only or Lazarus-only libraries/units, *
+ * 6. Don't use Delphi-only, FreePascal-only or Lazarus-only libraries/units, *
  *    but if needed, make it out-ifdef-able.                                  *
- * 6. No use of third-party libraries/units as possible, but if needed, make  *
+ * 7. No use of third-party libraries/units as possible, but if needed, make  *
  *    it out-ifdef-able.                                                      *
- * 7. Try to use const when possible.                                         *
- * 8. Make sure to comment out writeln, used while debugging.                 *
- * 9. Make sure the code compiles on 32-bit and 64-bit platforms (x86-32,     *
- *    x86-64, ARM, ARM64, etc.).                                              *
- * 10. Make sure the code runs on all platforms with Vulkan support           *
+ * 8. Try to use const when possible.                                         *
+ * 9. Make sure to comment out writeln, used while debugging.                 *
+ * 10. Make sure the code compiles on 32-bit and 64-bit platforms (x86-32,    *
+ *     x86-64, ARM, ARM64, etc.).                                             *
+ * 11. Make sure the code runs on all platforms with Vulkan support           *
  *                                                                            *
  ******************************************************************************)
 unit PasVulkan;
