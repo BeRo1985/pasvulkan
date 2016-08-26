35038d7e6ed342465eec79090da816702bb1a696
Benjamin Rosseaux <benjamin@rosseaux.de>
2016-08-26 04:25:58 +0200

    - More work on PasVulkan.pas

--- src/PasVulkan.pas
+++ src/PasVulkan.pas
@@ -1,7 +1,7 @@
 (******************************************************************************
  *                                 PasVulkan                                  *
  ******************************************************************************
- *                        Version 2016-08-26-03-42-0000                       *
+ *                        Version 2016-08-26-04-25-0000                       *
  ******************************************************************************
  *                                zlib license                                *
  *============================================================================*
@@ -297,6 +297,27 @@ type EVulkanException=class(Exception);
        destructor Destroy; override;
       published
        property ResultCode:TVkResult read fResultCode;
+     end;
+
+     TVulkanFormatSizeFlag=
+      (
+       vfsfPacked,
+       vfsfCompressed,
+       vfsfPalettized,
+       vfsfDepth,
+       vfsfStencil
+      );
+
+     TVulkanFormatSizeFlags=set of TVulkanFormatSizeFlag;
+
+     PVulkanFormatSize=^TVulkanFormatSize;
+     TVulkanFormatSize=record
+      Flags:TVulkanFormatSizeFlags;
+      PaletteSizeInBits:TVkUInt32;
+      BlockSizeInBits:TVkUInt32;
+      BlockWidth:TVkUInt32; // in texels
+      BlockHeight:TVkUInt32; // in texels
+      BlockDepth:TVkUInt32; // in texels
      end;
 
      TVulkanObject=class(TInterfacedObject);
@@ -2466,6 +2487,8 @@ function VulkanGetFormatFromOpenGLFormat(const pFormat,pType:TVkUInt32):TVkForma
 function VulkanGetFormatFromOpenGLType(const pType,pNumComponents:TVkUInt32;const pNormalized:boolean):TVkFormat;
 function VulkanGetFormatFromOpenGLInternalFormat(const pInternalFormat:TVkUInt32):TVkFormat;
 
+function VulkanGetFormatSize(const pFormat:TVkFormat):TVulkanFormatSize;
+
 function VulkanRoundUpToPowerOfTwo(Value:TVkSize):TVkSize;
 
 function VulkanErrorToString(const ErrorCode:TVkResult):TVulkanCharString;
@@ -4452,6 +4475,389 @@ begin
   end;
   else begin
    result:=VK_FORMAT_UNDEFINED;
+  end;
+ end;
+end;
+
+function VulkanGetFormatSize(const pFormat:TVkFormat):TVulkanFormatSize;
+begin
+ case pFormat of
+  VK_FORMAT_R4G4_UNORM_PACK8:begin
+   result.Flags:=[vfsfPacked];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=1*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R4G4B4A4_UNORM_PACK16,VK_FORMAT_B4G4R4A4_UNORM_PACK16,VK_FORMAT_R5G6B5_UNORM_PACK16,VK_FORMAT_B5G6R5_UNORM_PACK16,VK_FORMAT_R5G5B5A1_UNORM_PACK16,VK_FORMAT_B5G5R5A1_UNORM_PACK16,VK_FORMAT_A1R5G5B5_UNORM_PACK16:begin
+   result.Flags:=[vfsfPacked];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=2*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R8_UNORM,VK_FORMAT_R8_SNORM,VK_FORMAT_R8_USCALED,VK_FORMAT_R8_SSCALED,VK_FORMAT_R8_UINT,VK_FORMAT_R8_SINT,VK_FORMAT_R8_SRGB:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=1*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R8G8_UNORM,VK_FORMAT_R8G8_SNORM,VK_FORMAT_R8G8_USCALED,VK_FORMAT_R8G8_SSCALED,VK_FORMAT_R8G8_UINT,VK_FORMAT_R8G8_SINT,VK_FORMAT_R8G8_SRGB:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=2*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R8G8B8_UNORM,VK_FORMAT_R8G8B8_SNORM,VK_FORMAT_R8G8B8_USCALED,VK_FORMAT_R8G8B8_SSCALED,VK_FORMAT_R8G8B8_UINT,VK_FORMAT_R8G8B8_SINT,VK_FORMAT_R8G8B8_SRGB,VK_FORMAT_B8G8R8_UNORM,VK_FORMAT_B8G8R8_SNORM,VK_FORMAT_B8G8R8_USCALED,VK_FORMAT_B8G8R8_SSCALED,VK_FORMAT_B8G8R8_UINT,VK_FORMAT_B8G8R8_SINT,VK_FORMAT_B8G8R8_SRGB:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=3*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R8G8B8A8_UNORM,
+  VK_FORMAT_R8G8B8A8_SNORM,VK_FORMAT_R8G8B8A8_USCALED,VK_FORMAT_R8G8B8A8_SSCALED,VK_FORMAT_R8G8B8A8_UINT,VK_FORMAT_R8G8B8A8_SINT,VK_FORMAT_R8G8B8A8_SRGB,VK_FORMAT_B8G8R8A8_UNORM,VK_FORMAT_B8G8R8A8_SNORM,VK_FORMAT_B8G8R8A8_USCALED,VK_FORMAT_B8G8R8A8_SSCALED,VK_FORMAT_B8G8R8A8_UINT,VK_FORMAT_B8G8R8A8_SINT,VK_FORMAT_B8G8R8A8_SRGB:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=4*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_A8B8G8R8_UNORM_PACK32,VK_FORMAT_A8B8G8R8_SNORM_PACK32,VK_FORMAT_A8B8G8R8_USCALED_PACK32,VK_FORMAT_A8B8G8R8_SSCALED_PACK32,VK_FORMAT_A8B8G8R8_UINT_PACK32,VK_FORMAT_A8B8G8R8_SINT_PACK32,VK_FORMAT_A8B8G8R8_SRGB_PACK32:begin
+   result.Flags:=[vfsfPacked];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=4*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_A2R10G10B10_UNORM_PACK32,VK_FORMAT_A2R10G10B10_SNORM_PACK32,VK_FORMAT_A2R10G10B10_USCALED_PACK32,VK_FORMAT_A2R10G10B10_SSCALED_PACK32,VK_FORMAT_A2R10G10B10_UINT_PACK32,VK_FORMAT_A2R10G10B10_SINT_PACK32,VK_FORMAT_A2B10G10R10_UNORM_PACK32,VK_FORMAT_A2B10G10R10_SNORM_PACK32,VK_FORMAT_A2B10G10R10_USCALED_PACK32,VK_FORMAT_A2B10G10R10_SSCALED_PACK32,VK_FORMAT_A2B10G10R10_UINT_PACK32,VK_FORMAT_A2B10G10R10_SINT_PACK32:begin
+   result.Flags:=[vfsfPacked];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=4*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R16_UNORM,VK_FORMAT_R16_SNORM,VK_FORMAT_R16_USCALED,VK_FORMAT_R16_SSCALED,VK_FORMAT_R16_UINT,VK_FORMAT_R16_SINT,VK_FORMAT_R16_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=2*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R16G16_UNORM,VK_FORMAT_R16G16_SNORM,VK_FORMAT_R16G16_USCALED,VK_FORMAT_R16G16_SSCALED,VK_FORMAT_R16G16_UINT,VK_FORMAT_R16G16_SINT,VK_FORMAT_R16G16_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=4*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R16G16B16_UNORM,VK_FORMAT_R16G16B16_SNORM,VK_FORMAT_R16G16B16_USCALED,VK_FORMAT_R16G16B16_SSCALED,VK_FORMAT_R16G16B16_UINT,VK_FORMAT_R16G16B16_SINT,VK_FORMAT_R16G16B16_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=6*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R16G16B16A16_UNORM,VK_FORMAT_R16G16B16A16_SNORM,VK_FORMAT_R16G16B16A16_USCALED,VK_FORMAT_R16G16B16A16_SSCALED,VK_FORMAT_R16G16B16A16_UINT,VK_FORMAT_R16G16B16A16_SINT,VK_FORMAT_R16G16B16A16_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=8*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R32_UINT,VK_FORMAT_R32_SINT,VK_FORMAT_R32_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=4*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R32G32_UINT,VK_FORMAT_R32G32_SINT,VK_FORMAT_R32G32_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=8*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R32G32B32_UINT,VK_FORMAT_R32G32B32_SINT,VK_FORMAT_R32G32B32_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=12*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R32G32B32A32_UINT,VK_FORMAT_R32G32B32A32_SINT,VK_FORMAT_R32G32B32A32_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R64_UINT,VK_FORMAT_R64_SINT,VK_FORMAT_R64_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=8*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R64G64_UINT,VK_FORMAT_R64G64_SINT,VK_FORMAT_R64G64_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R64G64B64_UINT,VK_FORMAT_R64G64B64_SINT,VK_FORMAT_R64G64B64_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=24*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_R64G64B64A64_UINT,VK_FORMAT_R64G64B64A64_SINT,VK_FORMAT_R64G64B64A64_SFLOAT:begin
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=32*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_B10G11R11_UFLOAT_PACK32,VK_FORMAT_E5B9G9R9_UFLOAT_PACK32:begin
+   result.Flags:=[vfsfPacked];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=4*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_D16_UNORM:begin
+   result.Flags:=[vfsfDepth];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=2*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_X8_D24_UNORM_PACK32:begin
+   result.Flags:=[vfsfPacked,vfsfDepth];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=4*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_D32_SFLOAT:begin
+   result.Flags:=[vfsfDepth];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=4*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_S8_UINT:begin
+   result.Flags:=[vfsfStencil];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=1*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_D16_UNORM_S8_UINT:begin
+   result.Flags:=[vfsfDepth,vfsfStencil];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=3*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_D24_UNORM_S8_UINT:begin
+   result.Flags:=[vfsfDepth,vfsfStencil];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=4*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_D32_SFLOAT_S8_UINT:begin
+   result.Flags:=[vfsfDepth,vfsfStencil];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=8*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_BC1_RGB_UNORM_BLOCK,VK_FORMAT_BC1_RGB_SRGB_BLOCK,VK_FORMAT_BC1_RGBA_UNORM_BLOCK,VK_FORMAT_BC1_RGBA_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=8*8;
+   result.BlockWidth:=4;
+   result.BlockHeight:=4;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_BC2_UNORM_BLOCK,VK_FORMAT_BC2_SRGB_BLOCK,VK_FORMAT_BC3_UNORM_BLOCK,VK_FORMAT_BC3_SRGB_BLOCK,VK_FORMAT_BC4_UNORM_BLOCK,VK_FORMAT_BC4_SNORM_BLOCK,VK_FORMAT_BC5_UNORM_BLOCK,VK_FORMAT_BC5_SNORM_BLOCK,VK_FORMAT_BC6H_UFLOAT_BLOCK,VK_FORMAT_BC6H_SFLOAT_BLOCK,VK_FORMAT_BC7_UNORM_BLOCK,VK_FORMAT_BC7_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=4;
+   result.BlockHeight:=4;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ETC2_R8G8B8_UNORM_BLOCK,VK_FORMAT_ETC2_R8G8B8_SRGB_BLOCK,VK_FORMAT_ETC2_R8G8B8A1_UNORM_BLOCK,VK_FORMAT_ETC2_R8G8B8A1_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=8*8;
+   result.BlockWidth:=4;
+   result.BlockHeight:=4;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ETC2_R8G8B8A8_UNORM_BLOCK,VK_FORMAT_ETC2_R8G8B8A8_SRGB_BLOCK,VK_FORMAT_EAC_R11_UNORM_BLOCK,VK_FORMAT_EAC_R11_SNORM_BLOCK,VK_FORMAT_EAC_R11G11_UNORM_BLOCK,VK_FORMAT_EAC_R11G11_SNORM_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=4;
+   result.BlockHeight:=4;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_4x4_UNORM_BLOCK,VK_FORMAT_ASTC_4x4_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=4;
+   result.BlockHeight:=4;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_5x4_UNORM_BLOCK,VK_FORMAT_ASTC_5x4_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=5;
+   result.BlockHeight:=4;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_5x5_UNORM_BLOCK,VK_FORMAT_ASTC_5x5_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=5;
+   result.BlockHeight:=5;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_6x5_UNORM_BLOCK,VK_FORMAT_ASTC_6x5_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=6;
+   result.BlockHeight:=5;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_6x6_UNORM_BLOCK,VK_FORMAT_ASTC_6x6_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=6;
+   result.BlockHeight:=6;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_8x5_UNORM_BLOCK,VK_FORMAT_ASTC_8x5_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=8;
+   result.BlockHeight:=5;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_8x6_UNORM_BLOCK,VK_FORMAT_ASTC_8x6_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=8;
+   result.BlockHeight:=6;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_8x8_UNORM_BLOCK,VK_FORMAT_ASTC_8x8_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=8;
+   result.BlockHeight:=8;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_10x5_UNORM_BLOCK,VK_FORMAT_ASTC_10x5_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=10;
+   result.BlockHeight:=5;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_10x6_UNORM_BLOCK,VK_FORMAT_ASTC_10x6_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=10;
+   result.BlockHeight:=6;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_10x8_UNORM_BLOCK,VK_FORMAT_ASTC_10x8_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=10;
+   result.BlockHeight:=8;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_10x10_UNORM_BLOCK,VK_FORMAT_ASTC_10x10_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=10;
+   result.BlockHeight:=10;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_12x10_UNORM_BLOCK,VK_FORMAT_ASTC_12x10_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=12;
+   result.BlockHeight:=10;
+   result.BlockDepth:=1;
+  end;
+  VK_FORMAT_ASTC_12x12_UNORM_BLOCK,VK_FORMAT_ASTC_12x12_SRGB_BLOCK:begin
+   result.Flags:=[vfsfCompressed];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=16*8;
+   result.BlockWidth:=12;
+   result.BlockHeight:=12;
+   result.BlockDepth:=1;
+  end;
+  else begin 
+   result.Flags:=[];
+   result.PaletteSizeInBits:=0;
+   result.BlockSizeInBits:=0*8;
+   result.BlockWidth:=1;
+   result.BlockHeight:=1;
+   result.BlockDepth:=1;
   end;
  end;
 end;
