135c0ac654791cd9163303f5da43f23279d9800a
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-05-04 05:38:02 +0200

    - Added new initial examples project

---
+++ examples/examples.dpr
@@ -0,0 +1,44 @@
+program examples;
+{$ifdef fpc}
+ {$mode delphi}
+{$endif} 
+{$if defined(win32) or defined(win64)}
+ {$apptype console}
+{$ifend}
+
+// {$if defined(fpc) and defined(Unix)}cthreads,{$ifend}
+
+uses
+  {$if defined(fpc) and defined(Unix)}cthreads,{$ifend}
+  SysUtils,
+  Classes,
+  Vulkan in '..\src\Vulkan.pas',
+  PasVulkan in '..\src\PasVulkan.pas',
+  PasVulkanSDL2 in '..\src\PasVulkanSDL2.pas',
+  PasVulkanStaticLinking in '..\src\PasVulkanStaticLinking.pas',
+  PasVulkanApplication in '..\src\PasVulkanApplication.pas';
+
+procedure SDLMain;
+begin
+ VulkanApplication:=TVulkanApplication.Create;
+ try
+{$ifndef Release}
+  VulkanApplication.VulkanDebugging:=true;
+  VulkanApplication.VulkanValidation:=true;
+{$endif}
+  VulkanApplication.Title:='SDL Vulkan Examples Application';
+  VulkanApplication.Run;
+ finally
+  FreeAndNil(VulkanApplication);
+ end;
+end;
+
+begin
+ SDLMain;
+{$ifndef fpc}
+ if DebugHook<>0 then begin
+  writeln('Press return to exit . . . ');
+  readln;
+ end;
+{$endif}
+end.
--- examples/triangle/compileshaders.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:/VulkanSDK/1.0.46.0/Bin32/glslangValidator.exe" -V triangle.vert -o triangle_vert.spv6
-"C:/VulkanSDK/1.0.46.0/Bin32/glslangValidator.exe" -V triangle.frag -o triangle_frag.spv
-pause
