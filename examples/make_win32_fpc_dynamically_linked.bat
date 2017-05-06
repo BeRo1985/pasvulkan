8da5f2ee713826aa31b2c1e39702df99af1babdf
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-05-06 21:06:53 +0200

    - More make script stuff

--- examples/make_win32_fpc_dynamically_linked.bat
+++ examples/make_win32_fpc_dynamically_linked.bat
@@ -1,2 +1,14 @@
 @echo off 
+del *.a
+del *.ppu
+del *.o
+del .\..\src\*.a
+del .\..\src\*.ppu
+del .\..\src\*.o
 fpc -Sd -B -g -gl -dRELEASE "-oexamples_i386_win32_release_fpc_dynamically_linked.exe" examples.dpr
+del *.a
+del *.ppu
+del *.o
+del .\..\src\*.a
+del .\..\src\*.ppu
+del .\..\src\*.o
--- examples/make_win64_fpc_dynamically_linked.bat
+++ examples/make_win64_fpc_dynamically_linked.bat
@@ -1,2 +1,14 @@
 @echo off 
+del *.a
+del *.ppu
+del *.o
+del .\..\src\*.a
+del .\..\src\*.ppu
+del .\..\src\*.o
 ppcrossx64 -Sd -B -g -gl -dRELEASE "-oexamples_x86_64_win64_release_fpc_dynamically_linked.exe" examples.dpr
+del *.a
+del *.ppu
+del *.o
+del .\..\src\*.a
+del .\..\src\*.ppu
+del .\..\src\*.o
