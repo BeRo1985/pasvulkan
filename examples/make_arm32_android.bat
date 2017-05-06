c62bab6ba5c3258a6c1280db76b5be48e1223542
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-05-06 22:09:09 +0200

    - More work

--- examples/make_arm32_android.bat
+++ examples/make_arm32_android.bat
@@ -7,9 +7,10 @@ del *.o
 del .\..\src\*.a
 del .\..\src\*.ppu
 del .\..\src\*.o
+del libmain.so 
 del .\libSDL2.so
 copy .\..\libs\sdl20androidarm32\libSDL2.so libSDL2.so
-"c:\FPC\3.1.1\bin\i386-win32\ppcrossarm.exe" -Tandroid -Sd -B -g -gl -dRELEASE -CpARMv7A -CfVFPv3 -O2 "-olibexamples.so" examples.dpr
+"c:\FPC\3.1.1\bin\i386-win32\ppcrossarm.exe" -Tandroid -Sd -B -g -gl -dRELEASE -CpARMv7A -CfVFPv3 -O2 "-olibmain.so" examples.dpr
 del *.a
 del *.ppu
 del *.o
