1b77ec1b64c1bf4299128eb55624e84b87a0d79b
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-05-06 21:01:54 +0200

    - More make script stuff

--- examples/make_win32_fpc_statically_linked.bat
+++ examples/make_win32_fpc_statically_linked.bat
@@ -1,17 +1,12 @@
 @echo off 
 SET PATH=c:\FPC\3.1.1\bin\i386-win32\
 SET nodosfilewarning=1
-del ..\bin\furbenchnongui_i386_win32_release.exe
+del examples_i386_win32_release_fpc_dynamically_linked.exe
 del *.a
 del *.ppu
 del *.o
-del ..\bin\*.a
-del ..\bin\*.o
-del ..\bin\*.ppu
 copy ..\libs\sdl20win32\*.a *.a
 "c:\FPC\3.1.1\bin\i386-win32\fpc" -Sd -B -O3 -g -gl -CfSSE -Sd -dSTATICLINK -dRELEASE -dc_int64 "-oexamples_i386_win32_release_fpc_statically_linked.exe" examples.dpr
 del *.a
 del *.ppu
 del *.o
-del ..\bin\*.a
-del ..\bin\*.o
---
+++ examples/make_win64_fpc_statically_linked.bat
@@ -0,0 +1,12 @@
+@echo off 
+SET PATH=c:\FPC\3.1.1\bin\i386-win32\
+SET nodosfilewarning=1
+del examples_x86_64_win64_release_fpc_dynamically_linked.exe
+del *.a
+del *.ppu
+del *.o
+copy ..\libs\sdl20win64\*.a *.a
+"c:\FPC\3.1.1\bin\i386-win32\ppcrossx64" -Sd -B -O3 -g -gl -dSDL20 -dSTATICLINK -dUseCThreads -dRELEASE -k--allow-multiple-definition "-oexamples_x86_64_win64_release_fpc_statically_linked.exe" examples.dpr
+del *.a
+del *.ppu
+del *.o
