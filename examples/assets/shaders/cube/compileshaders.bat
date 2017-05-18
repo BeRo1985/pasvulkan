0afeab9d45a10e24e1c5d2ba3cbc89a20985abd6
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-05-18 23:54:04 +0200

    - More work

--- examples/assets/shaders/cube/compileshaders.bat
+++ examples/assets/shaders/cube/compileshaders.bat
@@ -1,4 +1,4 @@
 @echo off
-"C:/VulkanSDK/1.0.46.0/Bin32/glslangValidator.exe" -V cube.vert -o cube_vert.spv
-"C:/VulkanSDK/1.0.46.0/Bin32/glslangValidator.exe" -V cube.frag -o cube_frag.spv
+"%VULKAN_SDK%/Bin32/glslangValidator.exe" -V cube.vert -o cube_vert.spv
+"%VULKAN_SDK%/Bin32/glslangValidator.exe" -V cube.frag -o cube_frag.spv
 
--- examples/assets/shaders/dragon/compileshaders.bat
+++ examples/assets/shaders/dragon/compileshaders.bat
@@ -1,4 +1,4 @@
 @echo off
-"C:/VulkanSDK/1.0.46.0/Bin32/glslangValidator.exe" -V dragon.vert -o dragon_vert.spv
-"C:/VulkanSDK/1.0.46.0/Bin32/glslangValidator.exe" -V dragon.frag -o dragon_frag.spv
+"%VULKAN_SDK%/Bin32/glslangValidator.exe" -V dragon.vert -o dragon_vert.spv
+"%VULKAN_SDK%/Bin32/glslangValidator.exe" -V dragon.frag -o dragon_frag.spv
 
