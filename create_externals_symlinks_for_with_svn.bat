b30ef3e64b94131f850dd152a5275bc0e7dede9f
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-06-27 05:16:22 +0200

    - Improved create_externals_symlinks_for_with_svn.bat

--- create_externals_symlinks_for_with_svn.bat
+++ create_externals_symlinks_for_with_svn.bat
@@ -1,5 +1,6 @@
 @echo off
-mklink /J externals\pasmp ..\..\PASMP.github\trunk
-mklink /J externals\pucu ..\..\PUCU.github\trunk
-mklink /J externals\pasdblstrutils ..\..\PASDBLSTRUTILS.github\trunk
-mklink /J externals\kraft ..\..\KRAFT.github\trunk
+if not exist externals mkdir externals 2>nul
+if not exist externals\pasmp mklink /J externals\pasmp ..\..\PASMP.github\trunk
+if not exist externals\pucu mklink /J externals\pucu ..\..\PUCU.github\trunk
+if not exist externals\pasdblstrutils mklink /J externals\pasdblstrutils ..\..\PASDBLSTRUTILS.github\trunk
+if not exist externals\kraft mklink /J externals\kraft ..\..\KRAFT.github\trunk
