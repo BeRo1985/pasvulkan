a50887d54f9786a6e75eeda895a7e7ca77f4586b
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-06-27 05:11:56 +0200

    - Added create_externals_symlinks_for_with_svn.bat for SVN users like me

---
+++ create_externals_symlinks_for_with_svn.bat
@@ -0,0 +1,5 @@
+@echo off
+mklink /J externals\pasmp ..\..\PASMP.github\trunk
+mklink /J externals\pucu ..\..\PUCU.github\trunk
+mklink /J externals\pasdblstrutils ..\..\PASDBLSTRUTILS.github\trunk
+mklink /J externals\kraft ..\..\KRAFT.github\trunk
