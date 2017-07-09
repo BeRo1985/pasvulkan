8944920250a883def199f1d1f4fbc0c329fab92f
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-07-09 10:21:04 +0200

    - More work

--- examples/make_win32_fpc_statically_linked.bat
+++ examples/make_win32_fpc_statically_linked.bat
@@ -9,7 +9,7 @@ del .\..\src\*.a
 del .\..\src\*.ppu
 del .\..\src\*.o
 copy ..\libs\sdl20win32\*.a *.a
-"c:\FPC\3.1.1\bin\i386-win32\fpc" -Sd -B -O3 -g -gl -CfSSE -Sd -dSTATICLINK -dRELEASE -dc_int64 "-oexamples_i386_win32_release_fpc_statically_linked.exe" examples.dpr
+"c:\FPC\3.1.1\bin\i386-win32\fpc" -Sd -B -O3 -g -gl -CfSSE -Sd -dSTATICLINK -dRELEASE -dPasVulkanPasMP -dc_int64 "-oexamples_i386_win32_release_fpc_statically_linked.exe" examples.dpr
 del *.a
 del *.ppu
 del *.o
--- examples/make_win64_fpc_statically_linked.bat
+++ examples/make_win64_fpc_statically_linked.bat
@@ -9,7 +9,7 @@ del *.a
 del *.ppu
 del *.o
 copy ..\libs\sdl20win64\*.a *.a
-"c:\FPC\3.1.1\bin\i386-win32\ppcrossx64" -Sd -B -O3 -g -gl -dSDL20 -dSTATICLINK -dUseCThreads -dRELEASE -k--allow-multiple-definition "-oexamples_x86_64_win64_release_fpc_statically_linked.exe" examples.dpr
+"c:\FPC\3.1.1\bin\i386-win32\ppcrossx64" -Sd -B -O3 -g -gl -dSDL20 -dSTATICLINK -dRELEASE -dPasVulkanPasMP -k--allow-multiple-definition "-oexamples_x86_64_win64_release_fpc_statically_linked.exe" examples.dpr
 del *.a
 del *.ppu
 del *.o
