499f21b6c42262adc64fe63aaa86d72c1f746f66
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-07-16 06:35:07 +0200

    - More work

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -5,7 +5,7 @@
 2. The zlib license header goes at the top of each source file, with appropriate copyright notice.
 3. This PasVulkan wrapper may be used only with the PasVulkan-own Vulkan Pascal header.
 4. After a pull request, check the status of your pull request on http://github.com/BeRo1985/pasvulkan
-5. Write code, which is compatible with Delphi 7-XE7 and FreePascal >= 3.0 so don't use generics/templates, operator overloading and another newer syntax features than Delphi 7 has support for that, but if needed, make it out-ifdef-able.
+5. Write code which's compatible with Delphi >= 2009 and FreePascal >= 3.0
 6. Don't use Delphi-only, FreePascal-only or Lazarus-only libraries/units, but if needed, make it out-ifdef-able.
 7. No use of third-party libraries/units as possible, but if needed, make it out-ifdef-able.
 8. Try to use const when possible.
--- src/PasVulkan.Android.pas
+++ src/PasVulkan.Android.pas
@@ -36,10 +36,7 @@
  *    Pascal header.                                                          *
  * 4. After a pull request, check the status of your pull request on          *
       http://github.com/BeRo1985/pasvulkan                                    *
- * 5. Write code, which is compatible with Delphi 7-XE7 and FreePascal >= 3.0 *
- *    so don't use generics/templates, operator overloading and another newer *
- *    syntax features than Delphi 7 has support for that, but if needed, make *
- *    it out-ifdef-able.                                                      *
+ * 5. Write code which's compatible with Delphi >= 2009 and FreePascal >= 3.0 *
  * 6. Don't use Delphi-only, FreePascal-only or Lazarus-only libraries/units, *
  *    but if needed, make it out-ifdef-able.                                  *
  * 7. No use of third-party libraries/units as possible, but if needed, make  *
