d98c9bd1c1b7b96aa1f772d603031634ec9ba684
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-11-10 06:59:59 +0100

    - More work

--- examples/make_arm32_android_release.bat
+++ examples/make_arm32_android_release.bat
@@ -13,7 +13,7 @@ del "android\\app\\src\\main\\jniLibs\\armeabi-v7a\\libmain.so"
 del libmain.so
 copy .\..\libs\sdl20androidarm32\libSDL2.so libSDL2.so
 copy .\..\libs\libpngandroid\obj\local\armeabi-v7a\libpng.a libpng.a
-"c:\FPC\3.1.1\bin\i386-win32\ppcrossarm.exe" -Tandroid -Sd -B -CpARMv7A -CfVFPv3 -OpARMv7a -OoFastMath -O1 -XX -Xs -CX -Cg "-olibmain.so" -dPasVulkanPasMP -dPasVulkanUseSDL2 examples.dpr
+"c:\FPC\3.1.1\bin\i386-win32\ppcrossarm.exe" -Tandroid -Sd -B -CpARMv7A -CfVFPv3 -OpARMv7a -OoFastMath -O1 -XX -Xs -CX -Cg "-olibmain.so" -dRELEASE -dPasVulkanPasMP -dPasVulkanUseSDL2 examples.dpr
 copy libmain.so "android\\app\\src\\main\\jniLibs\\armeabi-v7a\\libmain.so"
 del libpng.a
 del *.a
