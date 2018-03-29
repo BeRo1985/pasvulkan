705939a1e61590037022941fde4c6e1a8e37a1ec
Benjamin Rosseaux <benjamin@rosseaux.de>
2018-03-29 07:16:08 +0200

    - More work

--- examples/make_i386_android_debug.bat
+++ examples/make_i386_android_debug.bat
@@ -15,7 +15,7 @@ del libSDL2.so
 del "android\\app\\src\\main\\jniLibs\\x86\\libmain.so"
 del libmain.so
 copy .\..\libs\sdl20androidi386\libSDL2.so libSDL2.so
-copy .\..\libs\libpngandroid\obj\local\armeabi-v7a\libpng.a libpng.a
+copy .\..\libs\libpngandroid\obj\local\x86\libpng.a libpng.a
 "c:\FPC\3.1.1\bin\i386-win32\ppcross386.exe" -Tandroid -Sd -B -b -g -gl -gw3 -O- -O1 -XX -Xm -CX -Cg "-olibmain.so" "-FUlib\i386-android" -dDEBUG -dPasVulkanPasMP -dPasVulkanUseSDL2 -dCompileForWithPIC examples.dpr
 copy libmain.so "android\\app\\src\\main\\jniLibs\\x86\\libmain.so"
 del libpng.a
