6dd8a078e799d6541c254c69caeccd34c10e417f
Benjamin Rosseaux <benjamin@rosseaux.de>
2018-05-27 11:56:51 +0200

    - More work

--- examples/make_x86_32_win32_fpc_statically_linked.bat
+++ examples/make_x86_32_win32_fpc_statically_linked.bat
@@ -1,6 +1,6 @@
 @echo off 
 SET PATH=c:\FPC\3.1.1\bin\i386-win32\
 SET nodosfilewarning=1
-"c:\FPC\3.1.1\bin\i386-win32\fpc" -Sd -B -O3 -g -gl -CfSSE -Sd "-FUFPCOutput\x86_32-win32" "-FEFPCOutput\x86_32-win32" "-Fl..\libs\sdl20win32" "-Fo..\libs\sdl20win32" -dSTATICLINK -dRELEASE -dPasVulkanPasMP -dPasVulkanUseSDL2 -dc_int64 "-oexamples_x86_32_win32_release_fpc_statically_linked.exe" examples.dpr
+"c:\FPC\3.1.1\bin\i386-win32\fpc" -Sd -B -O- -O1 -g -gl -CfSSE -Sd "-FUFPCOutput\x86_32-win32" "-FEFPCOutput\x86_32-win32" "-Fl..\libs\sdl20win32" "-Fo..\libs\sdl20win32" -dSTATICLINK -dRELEASE -dPasVulkanPasMP -dPasVulkanUseSDL2 -dc_int64 "-oexamples_x86_32_win32_release_fpc_statically_linked.exe" examples.dpr
 copy /y "FPCOutput\\x86_32-win32\\examples_x86_32_win32_release_fpc_statically_linked.exe" examples_x86_32_win32_release_fpc_statically_linked.exe
 
--- examples/make_x86_64_win64_fpc_statically_linked.bat
+++ examples/make_x86_64_win64_fpc_statically_linked.bat
@@ -1,19 +1,5 @@
 @echo off 
-del .\..\src\*.a
-del .\..\src\*.ppu
-del .\..\src\*.o
 SET PATH=c:\FPC\3.1.1\bin\i386-win32\
 SET nodosfilewarning=1
-del examples_x86_64_win64_release_fpc_statically_linked.exe
-del *.a
-del *.ppu
-del *.o
-copy ..\libs\sdl20win64\*.a *.a
-"c:\FPC\3.1.1\bin\i386-win32\ppcrossx64" -Sd -B -O3 -g -gl -dSDL20 -dSTATICLINK -dRELEASE -dPasVulkanPasMP -dPasVulkanUseSDL2 -k--allow-multiple-definition "-oexamples_x86_64_win64_release_fpc_statically_linked.exe" examples.dpr
-del *.a
-del *.ppu
-del *.o
-del .\..\src\*.a
-del .\..\src\*.ppu
-del .\..\src\*.o
-
+"c:\FPC\3.1.1\bin\i386-win32\ppcrossx64" -Sd -B -O- -O1 -g -gl -Sd "-FUFPCOutput\x86_32-win32" "-FEFPCOutput\x86_32-win32" "-Fl..\libs\sdl20win64" "-Fo..\libs\sdl20win64" -dSTATICLINK -dRELEASE -dPasVulkanPasMP -dPasVulkanUseSDL2 -dc_int64 -k--allow-multiple-definition "-oexamples_x86_64_win64_release_fpc_statically_linked.exe" examples.dpr
+copy /y "FPCOutput\\x86_32-win32\\examples_x86_64_win64_release_fpc_statically_linked.exe" examples_x86_64_win64_release_fpc_statically_linked.exe
