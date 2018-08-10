6a243ff7fc8ef4b00aa1f8c3c99948fd844fcee8
Benjamin Rosseaux <benjamin@rosseaux.de>
2018-08-10 08:54:29 +0200

    - More work

--- create_externals_symlinks_for_with_svn.bat
+++ create_externals_symlinks_for_with_svn.bat
@@ -5,3 +5,6 @@ if not exist externals\pucu mklink /J externals\pucu ..\..\PUCU.github\trunk
 if not exist externals\pasdblstrutils mklink /J externals\pasdblstrutils ..\..\PASDBLSTRUTILS.github\trunk
 if not exist externals\kraft mklink /J externals\kraft ..\..\KRAFT.github\trunk
 if not exist externals\pasjson mklink /J externals\pasjson ..\..\PASJSON.github\trunk
+if not exist externals\pasgltf mklink /J externals\pasgltf ..\..\PASGLTF.github\trunk
+if not exist externals\rnl mklink /J externals\rnl ..\..\RNL.github\trunk
+
