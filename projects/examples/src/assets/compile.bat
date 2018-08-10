a60a84f6a32c3f8911ca911529d511998850a6f8
Benjamin Rosseaux <benjamin@rosseaux.de>
2018-08-10 16:48:39 +0200

    - More work

--- projects/examples/src/assets/compile.bat
+++ projects/examples/src/assets/compile.bat
@@ -3,3 +3,4 @@ rem Compile here your asset files and move the compiled files then to ..\..\asse
 cd shaders
 call compileshaders.bat
 cd ..
+tools\dae2mdl\dae2mdl.exe models\dragon.dae ..\..\assets\models\dragon.mdl
