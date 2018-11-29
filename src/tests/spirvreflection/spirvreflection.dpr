ec1fb3e18b5f782470af9cc4486f9c88a2eafdb4
Benjamin Rosseaux <benjamin@rosseaux.de>
2018-11-29 01:12:51 +0100

    - More work

---
+++ src/tests/spirvreflection/spirvreflection.dpr
@@ -0,0 +1,23 @@
+program spirvreflection;
+{$ifdef fpc}
+ {$mode delphi}
+{$else}
+ {$legacyifend on}
+{$endif}
+{$if defined(Win32) or defined(Win64) or defined(Windows)}
+ {$apptype console}
+{$ifend}
+
+uses
+  System.SysUtils,
+  PasVulkan.Framework in '..\..\PasVulkan.Framework.pas';
+
+begin
+ try
+  readln;
+ except
+  on E:Exception do begin
+   Writeln(E.ClassName, ': ', E.Message);
+  end;
+ end;
+end.
