82820399dd3b4a2b05f13b298325758669019b07
Benjamin Rosseaux <benjamin@rosseaux.de>
2022-06-11 23:38:22 +0200

    Work on more OIT modes

--- projects/gltftest/src/UnitScreenMain.pas
+++ projects/gltftest/src/UnitScreenMain.pas
@@ -3481,7 +3481,7 @@ inherited Create(aFrameGraph);
                                     'weightblendedorderindependenttransparency_revealage',
                                     VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
                                     TpvFrameGraph.TLoadOp.Create(TpvFrameGraph.TLoadOp.TKind.Clear,
-                                                                 TpvVector4.InlineableCreate(0.0,0.0,0.0,0.0)),
+                                                                 TpvVector4.InlineableCreate(1.0,1.0,1.0,1.0)),
                                     [TpvFrameGraph.TResourceTransition.TFlag.Attachment]
                                    );
 
