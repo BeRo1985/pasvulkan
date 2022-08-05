d57172ca89dd66b14552e4704ad937ee58b50055
Benjamin Rosseaux <benjamin@rosseaux.de>
2022-08-05 03:11:53 +0200

    More work

--- src/PasVulkan.Scene3D.Renderer.pas
+++ src/PasVulkan.Scene3D.Renderer.pas
@@ -64,6 +64,7 @@ interface
 
 uses Classes,
      SysUtils,
+     Math,
      PasMP,
      Vulkan,
      PasVulkan.Types,
@@ -76,6 +77,7 @@ uses Classes,
      PasVulkan.Collections,
      PasVulkan.CircularDoublyLinkedList,
      PasVulkan.Scene3D,
+     PasVulkan.Scene3D.Renderer.Globals,
      PasVulkan.Scene3D.Renderer.SMAAData,
      PasVulkan.Scene3D.Renderer.SkyCubeMap,
      PasVulkan.Scene3D.Renderer.SkyBox,
@@ -120,14 +122,51 @@ type TpvScene3DRenderer=class;
        fScene3D:TpvScene3D;
        fVulkanDevice:TpvVulkanDevice;
        fCountInFlightFrames:TpvSizeInt;
+       fAntialiasingMode:TpvScene3DRendererAntialiasingMode;
+       fShadowMode:TpvScene3DRendererShadowMode;
+       fTransparencyMode:TpvScene3DRendererTransparencyMode;
+       fMaxMSAA:TpvInt32;
+       fMaxShadowMSAA:TpvInt32;
+       fShadowMapSize:TpvInt32;
+       fBufferDeviceAddress:boolean;
+       fMeshFragTypeName:TpvUTF8String;
+       fMeshFragShadowTypeName:TpvUTF8String;
+       fOptimizedNonAlphaFormat:TVkFormat;
+       fUseDepthPrepass:boolean;
+       fUseDemote:boolean;
+       fUseNoDiscard:boolean;
+       fUseOITAlphaTest:boolean;
+       fShadowMapSampleCountFlagBits:TVkSampleCountFlagBits;
+       fCountCascadedShadowMapMSAASamples:TpvSizeInt;
+       fSurfaceSampleCountFlagBits:TVkSampleCountFlagBits;
+       fCountSurfaceMSAASamples:TpvSizeInt;
       public
        constructor Create(const aScene3D:TpvScene3D;const aVulkanDevice:TpvVulkanDevice=nil;const aCountInFlightFrames:TpvSizeInt=MaxInFlightFrames); reintroduce;
        destructor Destroy; override;
        class procedure SetupVulkanDevice(const aVulkanDevice:TpvVulkanDevice); static;
+       procedure Setup;
       published
        property Scene3D:TpvScene3D read fScene3D;
        property VulkanDevice:TpvVulkanDevice read fVulkanDevice;
        property CountInFlightFrames:TpvSizeInt read fCountInFlightFrames;
+       property AntialiasingMode:TpvScene3DRendererAntialiasingMode read fAntialiasingMode write fAntialiasingMode;
+       property ShadowMode:TpvScene3DRendererShadowMode read fShadowMode write fShadowMode;
+       property TransparencyMode:TpvScene3DRendererTransparencyMode read fTransparencyMode write fTransparencyMode;
+       property MaxMSAA:TpvInt32 read fMaxMSAA write fMaxMSAA;
+       property MaxShadowMSAA:TpvInt32 read fMaxShadowMSAA write fMaxShadowMSAA;
+       property ShadowMapSize:TpvInt32 read fShadowMapSize write fShadowMapSize;
+       property BufferDeviceAddress:boolean read fBufferDeviceAddress;
+       property MeshFragTypeName:TpvUTF8String read fMeshFragTypeName;
+       property MeshFragShadowTypeName:TpvUTF8String read fMeshFragShadowTypeName;
+       property OptimizedNonAlphaFormat:TVkFormat read fOptimizedNonAlphaFormat;
+       property UseDepthPrepass:boolean read fUseDepthPrepass;
+       property UseDemote:boolean read fUseDemote;
+       property UseNoDiscard:boolean read fUseNoDiscard;
+       property UseOITAlphaTest:boolean read fUseOITAlphaTest;
+       property ShadowMapSampleCountFlagBits:TVkSampleCountFlagBits read fShadowMapSampleCountFlagBits;
+       property CountCascadedShadowMapMSAASamples:TpvSizeInt read fCountCascadedShadowMapMSAASamples;
+       property SurfaceSampleCountFlagBits:TVkSampleCountFlagBits read fSurfaceSampleCountFlagBits;
+       property CountSurfaceMSAASamples:TpvSizeInt read fCountSurfaceMSAASamples;
      end;
 
 
@@ -220,10 +259,22 @@ begin
  if assigned(aVulkanDevice) then begin
   fVulkanDevice:=aVulkanDevice;
  end else begin
-  fVulkanDevice:=pvApplication.VulkanDevice;
+  fVulkanDevice:=fVulkanDevice;
  end;
 
  fCountInFlightFrames:=aCountInFlightFrames;
+
+ fAntialiasingMode:=TpvScene3DRendererAntialiasingMode.Auto;
+
+ fShadowMode:=TpvScene3DRendererShadowMode.Auto;
+
+ fTransparencyMode:=TpvScene3DRendererTransparencyMode.Auto;
+
+ fMaxMSAA:=0;
+
+ fMaxShadowMSAA:=0;
+
+ fShadowMapSize:=0;
 
 end;
 
@@ -276,6 +327,283 @@ begin
     (aVulkanDevice.PhysicalDevice.AvailableExtensionNames.IndexOf(VK_KHR_SPIRV_1_4_EXTENSION_NAME)>=0) then begin
   aVulkanDevice.EnabledExtensionNames.Add(VK_KHR_SPIRV_1_4_EXTENSION_NAME);
  end;
+end;
+
+procedure TpvScene3DRenderer.Setup;
+var SampleCounts:TVkSampleCountFlags;
+    FormatProperties:TVkFormatProperties;
+    fMaxShadowMSAA,MaxMSAA:TpvInt32;
+begin
+
+ if fShadowMapSize=0 then begin
+  fShadowMapSize:=512;
+ end;
+
+ fShadowMapSize:=Max(16,fShadowMapSize);
+
+ fBufferDeviceAddress:=(fVulkanDevice.PhysicalDevice.BufferDeviceAddressFeaturesKHR.bufferDeviceAddress<>VK_FALSE) and
+                       (fVulkanDevice.PhysicalDevice.BufferDeviceAddressFeaturesKHR.bufferDeviceAddressCaptureReplay<>VK_FALSE);
+ if fBufferDeviceAddress then begin
+  fMeshFragTypeName:='matbufref';
+ end else begin
+  fMeshFragTypeName:='matssbo';
+ end;
+
+ FormatProperties:=fVulkanDevice.PhysicalDevice.GetFormatProperties(VK_FORMAT_B10G11R11_UFLOAT_PACK32);
+ if //(fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU) and
+    ((FormatProperties.linearTilingFeatures and (TVkFormatFeatureFlags(VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT) or
+                                                 TVkFormatFeatureFlags(VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT) or
+                                                 TVkFormatFeatureFlags(VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT) or
+                                                 TVkFormatFeatureFlags(VK_FORMAT_FEATURE_TRANSFER_DST_BIT) or
+                                                 TVkFormatFeatureFlags(VK_FORMAT_FEATURE_TRANSFER_SRC_BIT)))=(TVkFormatFeatureFlags(VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT) or
+                                                                                                              TVkFormatFeatureFlags(VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT) or
+                                                                                                              TVkFormatFeatureFlags(VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT) or
+                                                                                                              TVkFormatFeatureFlags(VK_FORMAT_FEATURE_TRANSFER_DST_BIT) or
+                                                                                                              TVkFormatFeatureFlags(VK_FORMAT_FEATURE_TRANSFER_SRC_BIT))) and
+    ((FormatProperties.optimalTilingFeatures and (TVkFormatFeatureFlags(VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT) or
+                                                  TVkFormatFeatureFlags(VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT) or
+                                                  TVkFormatFeatureFlags(VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT) or
+                                                  TVkFormatFeatureFlags(VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT) or
+                                                  TVkFormatFeatureFlags(VK_FORMAT_FEATURE_TRANSFER_DST_BIT) or
+                                                  TVkFormatFeatureFlags(VK_FORMAT_FEATURE_TRANSFER_SRC_BIT)))=(TVkFormatFeatureFlags(VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT) or
+                                                                                                               TVkFormatFeatureFlags(VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT) or
+                                                                                                               TVkFormatFeatureFlags(VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT) or
+                                                                                                               TVkFormatFeatureFlags(VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT) or
+                                                                                                               TVkFormatFeatureFlags(VK_FORMAT_FEATURE_TRANSFER_DST_BIT) or
+                                                                                                               TVkFormatFeatureFlags(VK_FORMAT_FEATURE_TRANSFER_SRC_BIT))) then begin
+  fOptimizedNonAlphaFormat:=VK_FORMAT_B10G11R11_UFLOAT_PACK32;
+ end else begin
+  fOptimizedNonAlphaFormat:=VK_FORMAT_R16G16B16A16_SFLOAT;
+ end;
+
+ case TpvVulkanVendorID(fVulkanDevice.PhysicalDevice.Properties.vendorID) of
+  TpvVulkanVendorID.ImgTec,
+  TpvVulkanVendorID.ARM,
+  TpvVulkanVendorID.Qualcomm,
+  TpvVulkanVendorID.Vivante:begin
+   // Tile-based GPUs => Use no depth prepass, as it can be counterproductive for those
+   fUseDepthPrepass:=false;
+  end;
+  else begin
+   // Immediate-based GPUs => Use depth prepass, as for which it can bring an advantage
+   fUseDepthPrepass:=true;
+  end;
+ end;
+
+ fUseDemote:=fVulkanDevice.PhysicalDevice.ShaderDemoteToHelperInvocation;
+
+ case TpvVulkanVendorID(fVulkanDevice.PhysicalDevice.Properties.vendorID) of
+  TpvVulkanVendorID.Intel:begin
+   // Workaround for Intel (i)GPUs, which've problems with discarding fragments in 2x2 fragment blocks at alpha-test usage
+   fUseNoDiscard:=not fUseDemote;
+   fUseOITAlphaTest:=true;
+  end;
+  else begin
+   fUseNoDiscard:=false;
+   fUseOITAlphaTest:=false;
+  end;
+ end;
+
+ if fAntialiasingMode=TpvScene3DRendererAntialiasingMode.Auto then begin
+  case TpvVulkanVendorID(fVulkanDevice.PhysicalDevice.Properties.vendorID) of
+   TpvVulkanVendorID.AMD:begin
+    if fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU then begin
+     fAntialiasingMode:=TpvScene3DRendererAntialiasingMode.FXAA;
+    end else begin
+     fAntialiasingMode:=TpvScene3DRendererAntialiasingMode.SMAA;
+    end;
+   end;
+   TpvVulkanVendorID.NVIDIA:begin
+    if fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU then begin
+     fAntialiasingMode:=TpvScene3DRendererAntialiasingMode.FXAA;
+    end else begin
+     fAntialiasingMode:=TpvScene3DRendererAntialiasingMode.SMAA;
+    end;
+   end;
+   TpvVulkanVendorID.Intel:begin
+    if fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU then begin
+     fAntialiasingMode:=TpvScene3DRendererAntialiasingMode.FXAA;
+    end else begin
+     fAntialiasingMode:=TpvScene3DRendererAntialiasingMode.SMAA;
+    end;
+   end;
+   else begin
+    if fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU then begin
+     fAntialiasingMode:=TpvScene3DRendererAntialiasingMode.DSAA;
+    end else begin
+     fAntialiasingMode:=TpvScene3DRendererAntialiasingMode.FXAA;
+    end;
+   end;
+  end;
+ end;
+
+ SampleCounts:=fVulkanDevice.PhysicalDevice.Properties.limits.framebufferColorSampleCounts and
+               fVulkanDevice.PhysicalDevice.Properties.limits.framebufferDepthSampleCounts and
+               fVulkanDevice.PhysicalDevice.Properties.limits.framebufferStencilSampleCounts;
+
+ if fMaxShadowMSAA=0 then begin
+  case TpvVulkanVendorID(fVulkanDevice.PhysicalDevice.Properties.vendorID) of
+   TpvVulkanVendorID.AMD:begin
+    fMaxShadowMSAA:=1;
+   end;
+   TpvVulkanVendorID.NVIDIA:begin
+    if fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU then begin
+     fMaxShadowMSAA:=8;
+    end else begin
+     fMaxShadowMSAA:=1;
+    end;
+   end;
+   TpvVulkanVendorID.Intel:begin
+    fMaxShadowMSAA:=1;
+   end;
+   else begin
+    fMaxShadowMSAA:=1;
+   end;
+  end;
+ end;
+
+ if fMaxMSAA=0 then begin
+  case TpvVulkanVendorID(fVulkanDevice.PhysicalDevice.Properties.vendorID) of
+   TpvVulkanVendorID.AMD:begin
+    fMaxMSAA:=2;
+   end;
+   TpvVulkanVendorID.NVIDIA:begin
+    if fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU then begin
+     fMaxMSAA:=8;
+    end else begin
+     fMaxMSAA:=2;
+    end;
+   end;
+   TpvVulkanVendorID.Intel:begin
+    fMaxMSAA:=2;
+   end;
+   else begin
+    fMaxMSAA:=2;
+   end;
+  end;
+ end;
+
+ if fShadowMode=TpvScene3DRendererShadowMode.Auto then begin
+  fShadowMode:=TpvScene3DRendererShadowMode.PCF;
+ end;
+
+ if fShadowMode in [TpvScene3DRendererShadowMode.PCF,TpvScene3DRendererShadowMode.DPCF,TpvScene3DRendererShadowMode.PCSS] then begin
+  fMeshFragShadowTypeName:='pcfpcss';
+ end else begin
+  fMeshFragShadowTypeName:='msm';
+ end;
+
+ if fShadowMode=TpvScene3DRendererShadowMode.MSM then begin
+  if (fMaxShadowMSAA>=64) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_64_BIT))<>0) then begin
+   fShadowMapSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_64_BIT);
+   fCountCascadedShadowMapMSAASamples:=64;
+  end else if (fMaxShadowMSAA>=32) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_32_BIT))<>0) then begin
+   fShadowMapSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_32_BIT);
+   fCountCascadedShadowMapMSAASamples:=32;
+  end else if (fMaxShadowMSAA>=16) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_16_BIT))<>0) then begin
+   fShadowMapSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_16_BIT);
+   fCountCascadedShadowMapMSAASamples:=16;
+  end else if (fMaxShadowMSAA>=8) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_8_BIT))<>0) then begin
+   fShadowMapSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_8_BIT);
+   fCountCascadedShadowMapMSAASamples:=8;
+  end else if (fMaxShadowMSAA>=4) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_4_BIT))<>0) then begin
+   fShadowMapSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_4_BIT);
+   fCountCascadedShadowMapMSAASamples:=4;
+  end else if (fMaxShadowMSAA>=2) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_2_BIT))<>0) then begin
+   fShadowMapSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_2_BIT);
+   fCountCascadedShadowMapMSAASamples:=2;
+  end else begin
+   fShadowMapSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_1_BIT);
+   fCountCascadedShadowMapMSAASamples:=1;
+  end;
+ end else begin
+  fShadowMapSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_1_BIT);
+  fCountCascadedShadowMapMSAASamples:=1;
+ end;
+
+ if fAntialiasingMode=TpvScene3DRendererAntialiasingMode.MSAA then begin
+  if (fMaxMSAA>=64) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_64_BIT))<>0) then begin
+   fSurfaceSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_64_BIT);
+   fCountSurfaceMSAASamples:=64;
+  end else if (fMaxMSAA>=32) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_32_BIT))<>0) then begin
+   fSurfaceSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_32_BIT);
+   fCountSurfaceMSAASamples:=32;
+  end else if (fMaxMSAA>=16) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_16_BIT))<>0) then begin
+   fSurfaceSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_16_BIT);
+   fCountSurfaceMSAASamples:=16;
+  end else if (fMaxMSAA>=8) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_8_BIT))<>0) then begin
+   fSurfaceSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_8_BIT);
+   fCountSurfaceMSAASamples:=8;
+  end else if (fMaxMSAA>=4) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_4_BIT))<>0) then begin
+   fSurfaceSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_4_BIT);
+   fCountSurfaceMSAASamples:=4;
+  end else if (fMaxMSAA>=2) and ((SampleCounts and TVkSampleCountFlags(VK_SAMPLE_COUNT_2_BIT))<>0) then begin
+   fSurfaceSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_2_BIT);
+   fCountSurfaceMSAASamples:=2;
+  end else begin
+   fSurfaceSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_1_BIT);
+   fCountSurfaceMSAASamples:=1;
+   fAntialiasingMode:=TpvScene3DRendererAntialiasingMode.FXAA;
+  end;
+ end else begin
+  fSurfaceSampleCountFlagBits:=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_1_BIT);
+  fCountSurfaceMSAASamples:=1;
+ end;
+
+ if fTransparencyMode=TpvScene3DRendererTransparencyMode.Auto then begin
+  case TpvVulkanVendorID(fVulkanDevice.PhysicalDevice.Properties.vendorID) of
+   TpvVulkanVendorID.AMD:begin
+    if (fSurfaceSampleCountFlagBits=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_1_BIT)) and
+       (fVulkanDevice.EnabledExtensionNames.IndexOf(VK_EXT_POST_DEPTH_COVERAGE_EXTENSION_NAME)>0) then begin
+     // >= RDNA, since VK_EXT_post_depth_coverage exists just from RDNA on.
+     fTransparencyMode:=TpvScene3DRendererTransparencyMode.SPINLOCKOIT;
+    end else begin
+     if fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU then begin
+      fTransparencyMode:=TpvScene3DRendererTransparencyMode.WBOIT;
+     end else begin
+      fTransparencyMode:=TpvScene3DRendererTransparencyMode.LOOPOIT;
+     end;
+    end;
+   end;
+   TpvVulkanVendorID.NVIDIA:begin
+    if fVulkanDevice.EnabledExtensionNames.IndexOf(VK_EXT_POST_DEPTH_COVERAGE_EXTENSION_NAME)>0 then begin
+     if (fVulkanDevice.EnabledExtensionNames.IndexOf(VK_EXT_FRAGMENT_SHADER_INTERLOCK_EXTENSION_NAME)>0) and
+        fVulkanDevice.PhysicalDevice.FragmentShaderPixelInterlock then begin
+      fTransparencyMode:=TpvScene3DRendererTransparencyMode.INTERLOCKOIT;
+     end else begin
+      fTransparencyMode:=TpvScene3DRendererTransparencyMode.SPINLOCKOIT;
+     end;
+    end else begin
+     if fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU then begin
+      fTransparencyMode:=TpvScene3DRendererTransparencyMode.WBOIT;
+     end else begin
+      fTransparencyMode:=TpvScene3DRendererTransparencyMode.MBOIT;
+     end;
+    end;
+   end;
+   TpvVulkanVendorID.Intel:begin
+    if (fSurfaceSampleCountFlagBits=TVkSampleCountFlagBits(VK_SAMPLE_COUNT_1_BIT)) and
+       (fVulkanDevice.EnabledExtensionNames.IndexOf(VK_EXT_FRAGMENT_SHADER_INTERLOCK_EXTENSION_NAME)>0) and
+       fVulkanDevice.PhysicalDevice.FragmentShaderPixelInterlock then begin
+     fTransparencyMode:=TpvScene3DRendererTransparencyMode.INTERLOCKOIT;
+    end else begin
+     if fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU then begin
+      fTransparencyMode:=TpvScene3DRendererTransparencyMode.WBOIT;
+     end else begin
+      fTransparencyMode:=TpvScene3DRendererTransparencyMode.MBOIT;
+     end;
+    end;
+   end;
+   else begin
+    if fVulkanDevice.PhysicalDevice.Properties.deviceType=VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU then begin
+     fTransparencyMode:=TpvScene3DRendererTransparencyMode.Direct;
+    end else begin
+     fTransparencyMode:=TpvScene3DRendererTransparencyMode.WBOIT;
+    end;
+   end;
+  end;
+ end;
+
 end;
 
 end.
