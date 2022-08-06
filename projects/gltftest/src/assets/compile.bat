c85baa2b31ad6b4c1a46b0e037087e4f4f476ca6
Benjamin Rosseaux <benjamin@rosseaux.de>
2022-08-06 15:12:00 +0200

    More work

--- projects/gltftest/src/assets/compile.bat
+++ projects/gltftest/src/assets/compile.bat
@@ -1,5 +1,5 @@
 @echo off
 rem Compile here your asset files and move the compiled files then to ..\..\assets\ 
-cd shaders
-call compileshaders.bat
-cd ..
+rem cd shaders
+rem call compileshaders.bat
+rem cd ..
