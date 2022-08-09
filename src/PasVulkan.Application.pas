e33f1fe4552daa4652f924ea6ea8001a694fd826
Benjamin Rosseaux <benjamin@rosseaux.com>
2022-08-09 06:25:35 +0200

    More work

--- src/PasVulkan.Application.pas
+++ src/PasVulkan.Application.pas
@@ -5907,6 +5907,10 @@ begin
    fVulkanDevice.EnabledExtensionNames.Add(VK_EXT_SHADER_VIEWPORT_INDEX_LAYER_EXTENSION_NAME);
   end;
 
+  if fVulkanDevice.PhysicalDevice.AvailableExtensionNames.IndexOf(VK_EXT_HOST_QUERY_RESET_EXTENSION_NAME)>=0 then begin
+   fVulkanDevice.EnabledExtensionNames.Add(VK_EXT_HOST_QUERY_RESET_EXTENSION_NAME);
+  end;
+
   if fVulkanDebugging and
      fVulkanDebuggingEnabled and
      fVulkanValidation and
