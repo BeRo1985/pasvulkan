502d9c65df8951e3c5d3244b757552ff2d37791a
Benjamin Rosseaux <benjamin@rosseaux.com>
2022-08-10 05:26:14 +0200

    More work

--- src/assets/shaders/scene3d/bidirectional_tonemapping.glsl
+++ src/assets/shaders/scene3d/bidirectional_tonemapping.glsl
@@ -3,16 +3,22 @@
 
 // Bidirectional temporal tonemapping for antialiasing, upsampling, downsampling, and so on.
 
-float Luminance(vec3 color){
-    return dot(color, vec3(0.2125, 0.7154, 0.0721));
-}
-
 vec3 ApplyToneMapping(vec3 color){
-  return color / (Luminance(color) + 1.0);
+#if 1
+  // ACESFilmic - https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
+  return (color * ((2.51 * color) + vec3(0.03))) / (color * ((2.43 * color) + vec3(0.59)) + vec3(0.14));
+#else
+  return color / (dot(color, vec3(0.2125, 0.7154, 0.0721)) + 1.0);
+#endif
 }
 
 vec3 ApplyInverseToneMapping(vec3 color){
-  return color / max(1.0 - Luminance(color), 1e-4);
+#if 1
+  // ACESFilmic - https://www.wolframalpha.com/input?i=2.51y%5E2%2B.03y%3Dx%282.43y%5E2%2B.59y%2B.14%29+solve+for+y}
+  return (sqrt((-10127.0 * color * color) + (13702.0 * color) + vec3(9.0)) + (59.0 * color) - vec3(3.0)) / (502.0 - vec3(486.0 * color)); 
+#else
+  return color / max(1.0 - dot(color, vec3(0.2125, 0.7154, 0.0721)), 1e-4);
+#endif
 }
 
 vec4 ApplyToneMapping(vec4 color){
