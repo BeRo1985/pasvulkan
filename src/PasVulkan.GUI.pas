7f27e52f89a228138545eb4404bf3f57945c0c07
Benjamin Rosseaux <benjamin@rosseaux.com>
2022-12-16 03:42:49 +0100

    More work

--- src/PasVulkan.GUI.pas
+++ src/PasVulkan.GUI.pas
@@ -838,6 +838,8 @@ type TpvGUIObject=class;
        fIconSearch:TObject;
        fIconSearchNext:TObject;
        fIconReplace:TObject;
+       fIconCog:TObject;
+       fIconCogs:TObject;
        fIconChevronHeight:TpvFloat;
        fIconPopupMenuHeight:TpvFloat;
        fIconMenuRightHeight:TpvFloat;
@@ -983,6 +985,8 @@ type TpvGUIObject=class;
        property IconSearch:TObject read fIconSearch write fIconSearch;
        property IconSearchNext:TObject read fIconSearchNext write fIconSearchNext;
        property IconReplace:TObject read fIconReplace write fIconReplace;
+       property IconCog:TObject read fIconCog write fIconCog;
+       property IconCogs:TObject read fIconCogs write fIconCogs;
        property IconChevronHeight:TpvFloat read fIconChevronHeight write fIconChevronHeight;
        property IconPopupMenuHeight:TpvFloat read fIconPopupMenuHeight write fIconPopupMenuHeight;
        property IconMenuRightHeight:TpvFloat read fIconMenuRightHeight write fIconMenuRightHeight;
@@ -6522,6 +6526,8 @@ begin
  fIconSearch:=nil;
  fIconSearchNext:=nil;
  fIconReplace:=nil;
+ fIconCog:=nil;
+ fIconCogs:=nil;
  Setup;
 end;
 
@@ -7433,6 +7439,49 @@ begin
                                                                               2,
                                                                               1);
 
+  fIconCog:=fSignedDistanceFieldSpriteAtlas.LoadSignedDistanceFieldSprite('IconCog',
+                                                                          'M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65'+
+                                                                          ' 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05'+
+                                                                          ' 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18'+
+                                                                          ' 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22'+
+                                                                          '2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05'+
+                                                                          ' 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82'+
+                                                                          ' 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03'+
+                                                                          ' 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z',
+                                                                          48,
+                                                                          48,
+                                                                          48.0/24.0,
+                                                                          0.0,
+                                                                          0.0,
+                                                                          TpvVectorPathFillRule.NonZero,
+                                                                          true,
+                                                                          2,
+                                                                          1);
+
+  fIconCogs:=fSignedDistanceFieldSpriteAtlas.LoadSignedDistanceFieldSprite('IconCogs',
+                                                                           'M15.9,18.45C17.25,18.45 18.35,17.35 18.35,16C18.35,14.65 17.25,13.55 15.9,13.55C14.54,13.55 13.45,14.65 13.45,16C13.45,17.35'+
+                                                                           ' 14.54,18.45 15.9,18.45M21.1,16.68L22.58,17.84C22.71,17.95 22.75,18.13 22.66,18.29L21.26,20.71C21.17,20.86 21,20.92'+
+                                                                           ' 20.83,20.86L19.09,20.16C18.73,20.44 18.33,20.67 17.91,20.85L17.64,22.7C17.62,22.87 17.47,23 17.3,23H14.5C14.32,23'+
+                                                                           ' 14.18,22.87 14.15,22.7L13.89,20.85C13.46,20.67 13.07,20.44 12.71,20.16L10.96,20.86C10.81,20.92 10.62,20.86 10.54,20.71L9.14,18.29C9.05,18.13'+
+                                                                           ' 9.09,17.95 9.22,17.84L10.7,16.68L10.65,16L10.7,15.31L9.22,14.16C9.09,14.05 9.05,13.86 9.14,13.71L10.54,11.29C10.62,11.13 10.81,11.07'+
+                                                                           ' 10.96,11.13L12.71,11.84C13.07,11.56 13.46,11.32 13.89,11.15L14.15,9.29C14.18,9.13 14.32,9 14.5,9H17.3C17.47,9 17.62,9.13'+
+                                                                           ' 17.64,9.29L17.91,11.15C18.33,11.32 18.73,11.56 19.09,11.84L20.83,11.13C21,11.07 21.17,11.13 21.26,11.29L22.66,13.71C22.75,13.86'+
+                                                                           ' 22.71,14.05 22.58,14.16L21.1,15.31L21.15,16L21.1,16.68M6.69,8.07C7.56,8.07 8.26,7.37 8.26,6.5C8.26,5.63 7.56,4.92 6.69,4.92A1.58,1.58'+
+                                                                           ' 0 0,0 5.11,6.5C5.11,7.37 5.82,8.07 6.69,8.07M10.03,6.94L11,7.68C11.07,7.75 11.09,7.87 11.03,7.97L10.13,9.53C10.08,9.63 9.96,9.67'+
+                                                                           ' 9.86,9.63L8.74,9.18L8,9.62L7.81,10.81C7.79,10.92 7.7,11 7.59,11H5.79C5.67,11 5.58,10.92 5.56,10.81L5.4,9.62L4.64,9.18L3.5,9.63C3.41,9.67'+
+                                                                           ' 3.3,9.63 3.24,9.53L2.34,7.97C2.28,7.87 2.31,7.75 2.39,7.68L3.34,6.94L3.31,6.5L3.34,6.06L2.39,5.32C2.31,5.25 2.28,5.13 2.34,5.03L3.24,3.47C3.3,3.37'+
+                                                                           ' 3.41,3.33 3.5,3.37L4.63,3.82L5.4,3.38L5.56,2.19C5.58,2.08 5.67,2 5.79,2H7.59C7.7,2 7.79,2.08 7.81,2.19L8,3.38L8.74,3.82L9.86,3.37C9.96,3.33'+
+                                                                           ' 10.08,3.37 10.13,3.47L11.03,5.03C11.09,5.13 11.07,5.25 11,5.32L10.03,6.06L10.06,6.5L10.03,6.94Z',
+                                                                           48,
+                                                                           48,
+                                                                           48.0/24.0,
+                                                                           0.0,
+                                                                           0.0,
+                                                                           TpvVectorPathFillRule.NonZero,
+                                                                           true,
+                                                                           2,
+                                                                           1);
+
   if length(CacheStoragePath)>0 then begin
 
    fSignedDistanceFieldSpriteAtlas.SaveToFile(CacheStoragePath+'gui_signed_distance_field_spriteatlas.zip',true);
@@ -7506,6 +7555,8 @@ begin
   fIconSearch:=fSignedDistanceFieldSpriteAtlas.Sprites['IconSearch'];
   fIconSearchNext:=fSignedDistanceFieldSpriteAtlas.Sprites['IconSearchNext'];
   fIconReplace:=fSignedDistanceFieldSpriteAtlas.Sprites['IconReplace'];
+  fIconCog:=fSignedDistanceFieldSpriteAtlas.Sprites['IconCog'];
+  fIconCogs:=fSignedDistanceFieldSpriteAtlas.Sprites['IconCogs'];
 
  end;
 
