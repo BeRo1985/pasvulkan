cdba69744e5f101d85a3eafd74824813d6e3924a
Benjamin Rosseaux <benjamin@rosseaux.com>
2023-08-27 09:12:41 +0200

    More work

--- src/PasVulkan.Scene3D.Renderer.pas
+++ src/PasVulkan.Scene3D.Renderer.pas
@@ -788,7 +788,7 @@ begin
  end;
 
  if fGlobalIlluminatonMode=TpvScene3DRendererGlobalIlluminatonMode.Auto then begin
-  fGlobalIlluminatonMode:=TpvScene3DRendererGlobalIlluminatonMode.CameraReflectionProbe;//StaticEnvironmentMap;//
+  fGlobalIlluminatonMode:=TpvScene3DRendererGlobalIlluminatonMode.StaticEnvironmentMap;//CameraReflectionProbe;//
  end;
 
 
