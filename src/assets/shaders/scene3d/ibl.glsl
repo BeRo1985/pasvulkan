3a8425456aa36cc441c0c69f627c19a652975531
Benjamin Rosseaux <benjamin@rosseaux.com>
2023-10-01 21:19:22 +0200

    More work

--- src/assets/shaders/scene3d/ibl.glsl
+++ src/assets/shaders/scene3d/ibl.glsl
@@ -21,6 +21,38 @@ vec3 getCubeMapDirection(in vec2 uv,
                    (mix(-1.0, 1.0, uv.y) * yDir) +
                    zDir);
 }
+
+vec2 getCubeMapTexCoordAndFaceIndex(in vec3 direction, 
+                                    out int faceIndex){
+  vec3 absoluteDirection = abs(direction);
+	float magnitude;
+	vec2 uv;
+	if(all(greaterThanEqual(absoluteDirection.zz, absoluteDirection.xy))){
+		faceIndex = (direction.z < 0.0) ? 5 : 4;
+		uv = vec2((direction.z < 0.0) ? -direction.x : direction.x, -direction.y);
+		magnitude = absoluteDirection.z;
+	}else if(absoluteDirection.y >= absoluteDirection.x){
+		faceIndex = (direction.y < 0.0) ? 3 : 2;
+		uv = vec2(direction.x, (direction.y < 0.0) ? -direction.z : direction.z);
+		magnitude = absoluteDirection.y;
+	}else{
+		faceIndex = (direction.x < 0.0) ? 1 : 0;
+		uv = vec2((direction.x < 0.0) ? direction.z : -direction.z, -direction.y);
+		magnitude = absoluteDirection.x;
+	}       
+	return ((0.5 / magnitude) * uv) + vec2(0.5);
+}
+
+vec3 fixCubeMapLookup(in vec3 direction,
+                      in float lod, 
+                      in int cubeSize) {
+  float magnitude = max(max(abs(direction.x), abs(direction.y)), abs(direction.z)),
+        scale = 1.0 - (exp2(lod) / float(cubeSize)); // float(cubeSize - 1) / float(cubeSize);
+  return direction *
+         vec3((abs(direction.x) != magnitude) ? scale : 1.0, 
+              (abs(direction.y) != magnitude) ? scale : 1.0, 
+              (abs(direction.z) != magnitude) ? scale : 1.0);
+}
 /* clang-format on */
 
 vec2 Hammersley(const in int index, const in int numSamples) {
