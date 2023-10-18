be9eeac1ce1dfcd32cc3bb60626cd6c5cc9b7be0
Benjamin Rosseaux <benjamin@rosseaux.com>
2023-10-19 01:15:20 +0200

    More work

---
+++ src/assets/shaders/scene3d/voxelgriddata_uniforms.glsl
@@ -0,0 +1,7 @@
+  vec4 clipMaps[4]; // xyz = center in world-space, w = half-extent of a voxel 
+  vec4 cellSizes; // size of a voxel in world-space
+  uint gridSize; // number of voxels in a clipmap in a single dimension
+  uint countClipMaps; // maximum 4 clipmaps
+  uint hardwareConservativeRasterization; // 0 = false, 1 = true
+  uint maxGlobalFragmentCount; // maximum number of fragments per voxel globally
+  uint maxLocalFragmentCount; // maximum number of fragments per voxel locally
