c7d437517d5d2b05ee8cfe9b722eefda9cc21508
Benjamin Rosseaux <benjamin@rosseaux.com>
2023-11-13 06:52:21 +0100

    More work

---
+++ src/PasVulkan.Scene3D.Renderer.Passes.MeshCullPass1ComputePass.pas
@@ -0,0 +1,287 @@
+(******************************************************************************
+ *                                 PasVulkan                                  *
+ ******************************************************************************
+ *                       Version see PasVulkan.Framework.pas                  *
+ ******************************************************************************
+ *                                zlib license                                *
+ *============================================================================*
+ *                                                                            *
+ * Copyright (C) 2016-2020, Benjamin Rosseaux (benjamin@rosseaux.de)          *
+ *                                                                            *
+ * This software is provided 'as-is', without any express or implied          *
+ * warranty. In no event will the authors be held liable for any damages      *
+ * arising from the use of this software.                                     *
+ *                                                                            *
+ * Permission is granted to anyone to use this software for any purpose,      *
+ * including commercial applications, and to alter it and redistribute it     *
+ * freely, subject to the following restrictions:                             *
+ *                                                                            *
+ * 1. The origin of this software must not be misrepresented; you must not    *
+ *    claim that you wrote the original software. If you use this software    *
+ *    in a product, an acknowledgement in the product documentation would be  *
+ *    appreciated but is not required.                                        *
+ * 2. Altered source versions must be plainly marked as such, and must not be *
+ *    misrepresented as being the original software.                          *
+ * 3. This notice may not be removed or altered from any source distribution. *
+ *                                                                            *
+ ******************************************************************************
+ *                  General guidelines for code contributors                  *
+ *============================================================================*
+ *                                                                            *
+ * 1. Make sure you are legally allowed to make a contribution under the zlib *
+ *    license.                                                                *
+ * 2. The zlib license header goes at the top of each source file, with       *
+ *    appropriate copyright notice.                                           *
+ * 3. This PasVulkan wrapper may be used only with the PasVulkan-own Vulkan   *
+ *    Pascal header.                                                          *
+ * 4. After a pull request, check the status of your pull request on          *
+      http://github.com/BeRo1985/pasvulkan                                    *
+ * 5. Write code which's compatible with Delphi >= 2009 and FreePascal >=     *
+ *    3.1.1                                                                   *
+ * 6. Don't use Delphi-only, FreePascal-only or Lazarus-only libraries/units, *
+ *    but if needed, make it out-ifdef-able.                                  *
+ * 7. No use of third-party libraries/units as possible, but if needed, make  *
+ *    it out-ifdef-able.                                                      *
+ * 8. Try to use const when possible.                                         *
+ * 9. Make sure to comment out writeln, used while debugging.                 *
+ * 10. Make sure the code compiles on 32-bit and 64-bit platforms (x86-32,    *
+ *     x86-64, ARM, ARM64, etc.).                                             *
+ * 11. Make sure the code runs on all platforms with Vulkan support           *
+ *                                                                            *
+ ******************************************************************************)
+unit PasVulkan.Scene3D.Renderer.Passes.MeshCullPass1ComputePass;
+{$i PasVulkan.inc}
+{$ifndef fpc}
+ {$ifdef conditionalexpressions}
+  {$if CompilerVersion>=24.0}
+   {$legacyifend on}
+  {$ifend}
+ {$endif}
+{$endif}
+{$m+}
+
+interface
+
+uses SysUtils,
+     Classes,
+     Math,
+     Vulkan,
+     PasVulkan.Types,
+     PasVulkan.Math,
+     PasVulkan.Framework,
+     PasVulkan.Application,
+     PasVulkan.FrameGraph,
+     PasVulkan.Scene3D,
+     PasVulkan.Scene3D.Renderer.Globals,
+     PasVulkan.Scene3D.Renderer,
+     PasVulkan.Scene3D.Renderer.Instance;
+
+type { TpvScene3DRendererPassesMeshCullPass1ComputePass }
+     TpvScene3DRendererPassesMeshCullPass1ComputePass=class(TpvFrameGraph.TComputePass)
+      public
+       type TPushConstants=packed record
+             BaseDrawIndexedIndirectCommandIndex:TpvUInt32;
+             CountDrawIndexedIndirectCommands:TpvUInt32;
+             DrawCallIndex:TpvUInt32;
+             CountObjectIndices:TpvUInt32;
+             SkipCulling:TpvUInt32;
+            end;
+            PPushConstants=^TPushConstants;
+      private
+       fInstance:TpvScene3DRendererInstance;
+       fComputeShaderModule:TpvVulkanShaderModule;
+       fVulkanPipelineShaderStageCompute:TpvVulkanPipelineShaderStage;
+       fPipelineLayout:TpvVulkanPipelineLayout;
+       fPipeline:TpvVulkanComputePipeline;
+      public
+       constructor Create(const aFrameGraph:TpvFrameGraph;const aInstance:TpvScene3DRendererInstance); reintroduce;
+       destructor Destroy; override;
+       procedure AcquirePersistentResources; override;
+       procedure ReleasePersistentResources; override;
+       procedure AcquireVolatileResources; override;
+       procedure ReleaseVolatileResources; override;
+       procedure Update(const aUpdateInFlightFrameIndex,aUpdateFrameIndex:TpvSizeInt); override;
+       procedure Execute(const aCommandBuffer:TpvVulkanCommandBuffer;const aInFlightFrameIndex,aFrameIndex:TpvSizeInt); override;
+     end;
+
+implementation
+
+{ TpvScene3DRendererPassesMeshCullPass1ComputePass }
+
+constructor TpvScene3DRendererPassesMeshCullPass1ComputePass.Create(const aFrameGraph:TpvFrameGraph;const aInstance:TpvScene3DRendererInstance);
+begin
+ inherited Create(aFrameGraph);
+ fInstance:=aInstance;
+ Name:='MeshCullPass1ComputePass';
+end;
+
+destructor TpvScene3DRendererPassesMeshCullPass1ComputePass.Destroy;
+begin
+ inherited Destroy;
+end;
+
+procedure TpvScene3DRendererPassesMeshCullPass1ComputePass.AcquirePersistentResources;
+var Stream:TStream;
+begin
+
+ inherited AcquirePersistentResources;
+
+ Stream:=pvScene3DShaderVirtualFileSystem.GetFile('mesh_cull_pass1_comp.spv');
+ try
+  fComputeShaderModule:=TpvVulkanShaderModule.Create(fInstance.Renderer.VulkanDevice,Stream);
+  fInstance.Renderer.VulkanDevice.DebugUtils.SetObjectName(fComputeShaderModule.Handle,VK_OBJECT_TYPE_SHADER_MODULE,'TpvScene3DRendererPassesMeshCullPass1ComputePass.fComputeShaderModule');
+ finally
+  Stream.Free;
+ end;
+
+ fVulkanPipelineShaderStageCompute:=TpvVulkanPipelineShaderStage.Create(VK_SHADER_STAGE_COMPUTE_BIT,fComputeShaderModule,'main');
+
+end;
+
+procedure TpvScene3DRendererPassesMeshCullPass1ComputePass.ReleasePersistentResources;
+begin
+ FreeAndNil(fVulkanPipelineShaderStageCompute);
+ FreeAndNil(fComputeShaderModule);
+ inherited ReleasePersistentResources;
+end;
+
+procedure TpvScene3DRendererPassesMeshCullPass1ComputePass.AcquireVolatileResources;
+var Index:TpvSizeInt;
+begin
+
+ inherited AcquireVolatileResources;
+
+ fPipelineLayout:=TpvVulkanPipelineLayout.Create(fInstance.Renderer.VulkanDevice);
+ fPipelineLayout.AddPushConstantRange(TVkShaderStageFlags(VK_SHADER_STAGE_COMPUTE_BIT),0,SizeOf(TpvScene3DRendererPassesMeshCullPass1ComputePass.TPushConstants));
+ fPipelineLayout.AddDescriptorSetLayout(fInstance.MeshCullPass1ComputeVulkanDescriptorSetLayout);
+ fPipelineLayout.Initialize;
+
+ fInstance.Renderer.VulkanDevice.DebugUtils.SetObjectName(fPipelineLayout.Handle,VK_OBJECT_TYPE_PIPELINE_LAYOUT,'TpvScene3DRendererPassesMeshCullPass1ComputePass.fPipelineLayout');
+
+ fPipeline:=TpvVulkanComputePipeline.Create(fInstance.Renderer.VulkanDevice,
+                                            fInstance.Renderer.VulkanPipelineCache,
+                                            0,
+                                            fVulkanPipelineShaderStageCompute,
+                                            fPipelineLayout,
+                                            nil,
+                                            0);
+ fInstance.Renderer.VulkanDevice.DebugUtils.SetObjectName(fPipeline.Handle,VK_OBJECT_TYPE_PIPELINE,'TpvScene3DRendererPassesMeshCullPass1ComputePass.fPipeline');
+
+end;
+
+procedure TpvScene3DRendererPassesMeshCullPass1ComputePass.ReleaseVolatileResources;
+var Index:TpvSizeInt;
+begin
+ FreeAndNil(fPipeline);
+ FreeAndNil(fPipelineLayout);
+ inherited ReleaseVolatileResources;
+end;
+
+procedure TpvScene3DRendererPassesMeshCullPass1ComputePass.Update(const aUpdateInFlightFrameIndex,aUpdateFrameIndex:TpvSizeInt);
+begin
+ inherited Update(aUpdateInFlightFrameIndex,aUpdateFrameIndex);
+end;
+
+procedure TpvScene3DRendererPassesMeshCullPass1ComputePass.Execute(const aCommandBuffer:TpvVulkanCommandBuffer;const aInFlightFrameIndex,aFrameIndex:TpvSizeInt);
+var RenderPassIndex,
+    DrawChoreographyBatchRangeIndex,
+    PreviousInFlightFrameIndex:TpvSizeInt;
+    DrawChoreographyBatchRangeDynamicArray:TpvScene3D.PDrawChoreographyBatchRangeDynamicArray;
+    DrawChoreographyBatchRange:TpvScene3D.PDrawChoreographyBatchRange;
+    MemoryBarrier:TVkMemoryBarrier;
+    PushConstants:TpvScene3DRendererPassesMeshCullPass1ComputePass.TPushConstants;
+begin
+ inherited Execute(aCommandBuffer,aInFlightFrameIndex,aFrameIndex);
+
+ PreviousInFlightFrameIndex:=FrameGraph.DrawPreviousInFlightFrameIndex;
+
+ FillChar(MemoryBarrier,SizeOf(TVkMemoryBarrier),#0);
+ MemoryBarrier.sType:=VK_STRUCTURE_TYPE_MEMORY_BARRIER;
+ MemoryBarrier.srcAccessMask:=TVkAccessFlags(VK_ACCESS_INDIRECT_COMMAND_READ_BIT) or TVkAccessFlags(VK_ACCESS_INDEX_READ_BIT) or TVkAccessFlags(VK_ACCESS_VERTEX_ATTRIBUTE_READ_BIT) or TVkAccessFlags(VK_ACCESS_SHADER_READ_BIT) or TVkAccessFlags(VK_ACCESS_SHADER_WRITE_BIT);
+ MemoryBarrier.dstAccessMask:=TVkAccessFlags(VK_ACCESS_SHADER_READ_BIT) or TVkAccessFlags(VK_ACCESS_SHADER_WRITE_BIT) or TVkAccessFlags(VK_ACCESS_TRANSFER_WRITE_BIT);
+ aCommandBuffer.CmdPipelineBarrier(TVkPipelineStageFlags(VK_PIPELINE_STAGE_DRAW_INDIRECT_BIT) or
+                                   TVkPipelineStageFlags(VK_PIPELINE_STAGE_VERTEX_INPUT_BIT) or
+                                   TVkPipelineStageFlags(VK_PIPELINE_STAGE_VERTEX_SHADER_BIT) or
+                                   TVkPipelineStageFlags(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT),
+                                   TVkPipelineStageFlags(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT) or
+                                   TVkPipelineStageFlags(VK_PIPELINE_STAGE_TRANSFER_BIT),
+                                   0,
+                                   1,@MemoryBarrier,
+                                   0,nil,
+                                   0,nil);
+
+ aCommandBuffer.CmdFillBuffer(fInstance.PerInFlightFrameGPUDrawIndexedIndirectCommandCounterBuffers[aInFlightFrameIndex].Handle,0,VK_WHOLE_SIZE,0);
+
+ FillChar(MemoryBarrier,SizeOf(TVkMemoryBarrier),#0);
+ MemoryBarrier.sType:=VK_STRUCTURE_TYPE_MEMORY_BARRIER;
+ MemoryBarrier.srcAccessMask:=TVkAccessFlags(VK_ACCESS_TRANSFER_WRITE_BIT);
+ MemoryBarrier.dstAccessMask:=TVkAccessFlags(VK_ACCESS_SHADER_READ_BIT) or TVkAccessFlags(VK_ACCESS_SHADER_WRITE_BIT);
+ aCommandBuffer.CmdPipelineBarrier(TVkPipelineStageFlags(VK_PIPELINE_STAGE_TRANSFER_BIT),
+                                   TVkPipelineStageFlags(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT),
+                                   0,
+                                   1,@MemoryBarrier,
+                                   0,nil,
+                                   0,nil);
+
+ aCommandBuffer.CmdBindPipeline(VK_PIPELINE_BIND_POINT_COMPUTE,fPipeline.Handle);
+
+ aCommandBuffer.CmdBindDescriptorSets(VK_PIPELINE_BIND_POINT_COMPUTE,
+                                      fPipelineLayout.Handle,
+                                      0,
+                                      1,
+                                      @fInstance.MeshCullPass1ComputeVulkanDescriptorSets[aInFlightFrameIndex].Handle,
+                                      0,
+                                      nil);
+
+ for RenderPassIndex:=0 to TpvScene3D.MaxRenderPassIndices-1 do begin
+
+  if fInstance.PerInFlightFrameGPUCulledArray[aInFlightFrameIndex,RenderPassIndex] then begin
+
+   DrawChoreographyBatchRangeDynamicArray:=@fInstance.DrawChoreographyBatchRangeFrameBuckets[aInFlightFrameIndex,RenderPassIndex];
+
+   for DrawChoreographyBatchRangeIndex:=0 to DrawChoreographyBatchRangeDynamicArray.Count-1 do begin
+
+    DrawChoreographyBatchRange:=@DrawChoreographyBatchRangeDynamicArray.Items[DrawChoreographyBatchRangeIndex];
+
+    if DrawChoreographyBatchRange^.CountCommands>0 then begin
+
+     PushConstants.BaseDrawIndexedIndirectCommandIndex:=DrawChoreographyBatchRange^.FirstCommand;
+     PushConstants.CountDrawIndexedIndirectCommands:=DrawChoreographyBatchRange^.CountCommands;
+     PushConstants.CountObjectIndices:=fInstance.PerInFlightFrameGPUCountObjectIndicesArray[PreviousInFlightFrameIndex];
+     PushConstants.DrawCallIndex:=DrawChoreographyBatchRange^.Index;
+     PushConstants.SkipCulling:=1;
+
+     aCommandBuffer.CmdPushConstants(fPipelineLayout.Handle,
+                                     TVkShaderStageFlags(TVkShaderStageFlagBits.VK_SHADER_STAGE_COMPUTE_BIT),
+                                     0,
+                                     SizeOf(TpvScene3DRendererPassesMeshCullPass1ComputePass.TPushConstants),
+                                     @PushConstants);
+
+     aCommandBuffer.CmdDispatch((DrawChoreographyBatchRange^.CountCommands+255) shr 8,1,1);
+
+    end;
+
+   end;
+
+  end;
+
+ end;
+
+ FillChar(MemoryBarrier,SizeOf(TVkMemoryBarrier),#0);
+ MemoryBarrier.sType:=VK_STRUCTURE_TYPE_MEMORY_BARRIER;
+ MemoryBarrier.srcAccessMask:=TVkAccessFlags(VK_ACCESS_SHADER_READ_BIT) or TVkAccessFlags(VK_ACCESS_SHADER_WRITE_BIT);
+ MemoryBarrier.dstAccessMask:=TVkAccessFlags(VK_ACCESS_INDIRECT_COMMAND_READ_BIT) or TVkAccessFlags(VK_ACCESS_INDEX_READ_BIT) or TVkAccessFlags(VK_ACCESS_VERTEX_ATTRIBUTE_READ_BIT) or TVkAccessFlags(VK_ACCESS_SHADER_READ_BIT) or TVkAccessFlags(VK_ACCESS_SHADER_WRITE_BIT);
+ aCommandBuffer.CmdPipelineBarrier(TVkPipelineStageFlags(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT),
+                                   TVkPipelineStageFlags(VK_PIPELINE_STAGE_DRAW_INDIRECT_BIT) or
+                                   TVkPipelineStageFlags(VK_PIPELINE_STAGE_VERTEX_INPUT_BIT) or
+                                   TVkPipelineStageFlags(VK_PIPELINE_STAGE_VERTEX_SHADER_BIT) or
+                                   TVkPipelineStageFlags(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT),
+                                   0,
+                                   1,@MemoryBarrier,
+                                   0,nil,
+                                   0,nil);
+
+end;
+
+
+end.
