22497eea309a3944d3f764ead597ec0be2e0af93
Benjamin Rosseaux <benjamin@rosseaux.com>
2024-01-04 04:24:34 +0100

    More work

--- src/assets/shaders/scene3d/colorgrading.glsl
+++ src/assets/shaders/scene3d/colorgrading.glsl
@@ -2,8 +2,100 @@
 #define COLOR_GRADING_GLSL
 
 #include "colortemperature.glsl"
+#include "rec2020.glsl"
 
-const vec3 LinearRGBLuminanceWeighting = vec3(0.2126, 0.7152, 0.0722); // Rec. 709 / Linear RGB
+// Order of operations:
+// - Exposure
+// - White Balance
+// - Contrast
+// - Brightness
+// - Color Filter
+// - Saturation
+// - Tone Mapping
+// - Gamma
+//
+// - Exposure
+// - Night adaptation
+// - White balance
+// - Channel mixer
+// - Shadows/mid-tones/highlights
+// - Slope/offset/power (CDL)
+// - Contrast
+// - Vibrance
+// - Saturation
+// - Curves
+// - Tone mapping
+// - Luminance scaling
+// - Gamut mapping
+//
+// Here are the default color grading options:
+// - Exposure: 0.0
+// - Night adaptation: 0.0
+// - White balance: temperature 0, and tint 0
+// - Channel mixer: red {1,0,0}, green {0,1,0}, blue {0,0,1}
+// - Shadows/mid-tones/highlights: shadows {1,1,1,0}, mid-tones {1,1,1,0}, highlights {1,1,1,0}, ranges {0,0.333,0.550,1}
+// - Slope/offset/power: slope 1.0, offset 0.0, and power 1.0
+// - Contrast: 1.0
+// - Vibrance: 1.0
+// - Saturation: 1.0
+// - Curves: gamma {1,1,1}, midPoint {1,1,1}, and scale {1,1,1}
+// - Tone mapping: ACESLegacyToneMapper
+// - Luminance scaling: false
+// - Gamut mapping: false
+
+struct ColorGradingSettings {
+
+  // Exposure, night adaptation, white balance
+  vec4 exposureNightAndWhiteBalanceTemperatureTint; // x: exposure, y: night adaptation, z: white balance temperature, w: white balance tint
+  
+  // Channel mixer
+  vec4 channelMixerRed; // x: red, y: green, z: blue, w: unused
+  vec4 channelMixerGreen; // x: red, y: green, z: blue, w: unused
+  vec4 channelMixerBlue; // x: red, y: green, z: blue, w: unused
+
+  // Shadows/mid-tones/highlights 
+  vec4 shadows;
+  vec4 midtones;
+  vec4 highlights;
+  vec4 tonalRanges;
+
+  // ASC CDL
+  vec4 slope;
+  vec4 offset;
+  vec4 power;
+
+  // Contrast, vibrance, saturation
+  vec4 contrastVibranceSaturation; // x: contrast, y: vibrance, z: saturation, w: unused
+
+  // Curves
+  vec4 curvesGamma; // x: red, y: green, z: blue, w: unused
+  vec4 curvesMidPoint; // x: red, y: green, z: blue, w: unused
+  vec4 curvesScale; // x: red, y: green, z: blue, w: unused
+
+  // Luminance scaling, gamut mapping
+  ivec4 luminanceScalingGamutMapping; // x: luminance scaling, y: gamut mapping, z: unused, w: unused
+};
+
+ColorGradingSettings defaultColorColorGradingSettings = ColorGradingSettings(
+  vec4(0.0, 0.0, 0.0, 0.0), // exposureNightAndWhiteBalanceTemperatureTint
+  vec4(1.0, 0.0, 0.0, 0.0), // channelMixerRed
+  vec4(0.0, 1.0, 0.0, 0.0), // channelMixerGreen
+  vec4(0.0, 0.0, 1.0, 0.0), // channelMixerBlue
+  vec4(1.0, 1.0, 1.0, 0.0), // shadows
+  vec4(1.0, 1.0, 1.0, 0.0), // midtones
+  vec4(1.0, 1.0, 1.0, 0.0), // highlights
+  vec4(0.0, 0.333, 0.55, 1.0), // tonalRanges, defaults from DaVinci Resolve 
+  vec4(1.0, 1.0, 1.0, 0.0), // slope
+  vec4(0.0, 0.0, 0.0, 0.0), // offset
+  vec4(1.0, 1.0, 1.0, 0.0), // power
+  vec4(1.0, 1.0, 1.0, 0.0), // contrastVibranceSaturation
+  vec4(1.0, 1.0, 1.0, 0.0), // curvesGamma
+  vec4(1.0, 1.0, 1.0, 0.0), // curvesMidPoint
+  vec4(1.0, 1.0, 1.0, 0.0) // curvesScale
+  ivec4(0, 0, 0, 0) // luminanceScalingGamutMapping
+);
+
+const vec3 LinearRGBLuminanceWeighting = vec3(0.2126729, 0.7151522, 0.0721750); // Rec. 709 / Linear RGB
 
 vec3 applyColorHueRotation(vec3 c, float hueRotationAngle){ 
   vec3 hueRotationValues = vec3(0.57735, sin(vec2(radians(hueRotationAngle)) + vec2(0.0, 1.57079632679)));
@@ -15,7 +107,93 @@ vec3 applyColorSaturation(vec3 c, float saturation){
 }
 
 vec3 applyColorContrast(vec3 c, float contrast){
-  return mix(vec3(0.5), c, contrast);
+  return max(vec3(0.0), mix(vec3(0.5), c, contrast));
+}
+
+vec3 applyColorGamma(vec3 c, float gamma){
+  return max(vec3(0.0), pow(c, vec3(1.0 / gamma)));
+}
+
+vec3 applyColorGrading(vec3 color, const in ColorGradingSettings colorGradingSettings){
+  
+  // White balance in linear space
+  {
+    float x = 0.31271 - ((colorGradingSettings.exposureNightAndWhiteBalanceTemperatureTint.z * (1.0 / 6.0)) *  
+                         ((colorGradingSettings.exposureNightAndWhiteBalanceTemperatureTint.z < 0.0) ? 0.1 : 0.05));
+    float y = fma(colorGradingSettings.exposureNightAndWhiteBalanceTemperatureTint.w, 0.05 / 6.0, ((2.87 * x) - (3.0 * x * x)) - 0.27509507);
+    color = max(vec3(0.0),
+                mat3(2.85847, -1.62879, -0.0248910, -0.210182, 1.15820, 0.000324281, 0.0418120, -0.118169, 1.06867) *
+              ((mat3(0.390405, 0.549941, 0.00892632, 0.0708416, 0.963172, 0.00135775, 0.0231082, 0.128021, 0.936245) * color) *
+                (vec3(0.949237, 1.03542, 1.08728) / // D65 white point
+                (mat3(0.7328, 0.4296, -0.1624, -0.7036, 1.6975, 0.0061, 0.0030, 0.0136, 0.9834) * 
+                  ((vec3(x / y, 1, 1.0 - (x + y)) * 1.0) / vec2(1.0, y).yxy)
+                )
+                )
+              ));    
+  }
+
+  // From linear sRGB to linear Rec. 2020 color space
+  color = LinearSRGBToLinearRec2020Matrix * max(vec3(0.0), color);
+
+  // Channel mixer
+  color = vec3(
+    dot(color, colorGradingSettings.channelMixerRed.xyz),
+    dot(color, colorGradingSettings.channelMixerGreen.xyz),
+    dot(color, colorGradingSettings.channelMixerBlue.xyz)
+  );
+
+  // Shadows/mid-tones/highlights
+  {
+    float y = dot(color, LinearRec2020LuminanceWeights),
+          s = 1.0 - smoothstep(colorGradingSettings.tonalRanges.x, colorGradingSettings.tonalRanges.y, y),
+          h = smoothstep(colorGradingSettings.tonalRanges.z, colorGradingSettings.tonalRanges.w, y),
+          m = 1.0 - (s + h);
+    color = (color * s * colorGradingSettings.shadows.xyz) + 
+            (color * m * colorGradingSettings.midtones.xyz) +
+            (color * h * colorGradingSettings.highlights.xyz);:
+  }
+
+  { 
+    // Stuff which behaves better in log space
+
+    // Linear to Log Space
+    color = fma(log(fma(color, vec3(5.555556), vec3(0.047996))) * 0.43429448190325176, vec3(0.244161), vec3(0.386036));
+
+    // ASC CDL
+    color = fma(color, colorGradingSettings.slope.xyz, colorGradingSettings.offset.xyz);
+    color = mix(pow(color, colorGradingSettings.power.xyz), color, vec3(lessThanEqual(color, vec3(0.0))));
+
+    // Contrast
+    color = mix(vec3(0.4135884), color, colorGradingSettings.contrastVibranceSaturation.x);
+
+    // Log Space to Linear
+    color = max(vec3(0.0), fma(exp(2.302585092994046 * fma(color, vec3(4.095658192749866), vec3(-1.5810715060963874))), vec3(0.17999998560000113), vec3(-0.008639279308857654)));
+
+  }
+
+  // Vibrance
+  {
+    float s = (colorGradingSettings.contrastVibranceSaturation.y - 1.0) / (1.0 + exp(-3.0 * (color.x - max(color.y, color.z)))) + 1.0;
+    vec3 l = LinearRec2020LuminanceWeights * (1.0 - s);
+    color = vec3(dot(color, l + vec2(s, 0.0).xyy), dot(color, l + vec2(s, 0.0).yxy), dot(color, l + vec2(s, 0.0).yyx));    
+  }
+
+  // Saturation
+  color = max(vec3(0.0), mix(vec3(dot(color, LinearRec2020LuminanceWeights)), color, colorGradingSettings.contrastVibranceSaturation.z));
+  
+  // Curves - "Practical HDR and Wide Color Techniques in Gran Turismo SPORT", Uchimura 2018
+  {
+    vec3 d = vec3(1.0) / pow(colorGradingSettings.curvesMidPoint.xyz, colorGradingSettings.curvesGamma.xyz - vec3(1.0));
+    vec3 dark = pow(color, colorGradingSettings.curvesGamma.xyz) * d;
+    vec3 light = fma(color - colorGradingSettings.curvesMidPoint.xyz, colorGradingSettings.curvesScale.xyz, colorGradingSettings.curvesMidPoint.xyz);
+    color = mix(light, dark, vec3(lessThanEqual(color, colorGradingSettings.curvesMidPoint.xyz)));
+  }
+
+  // From linear Rec. 2020 color space to linear sRGB
+  color = LinearRec2020ToLinearSRGBMatrix * max(vec3(0.0), color);
+
+  return color;
+
 }
 
 #endif 
--- src/assets/shaders/scene3d/rec2020.glsl
+++ src/assets/shaders/scene3d/rec2020.glsl
@@ -2,7 +2,7 @@
 #define REC2020_GLSL
 
 const mat3 LinearRec2020ToLinearSRGBMatrix = mat3(
-	vec3(1.6605,-0.1246, -0.0182),
+	vec3(1.6605, -0.1246, -0.0182),
 	vec3(-0.5876, 1.1329, -0.1006),
 	vec3(-0.0728, -0.0083, 1.1187)
 );
@@ -12,5 +12,7 @@ const mat3 LinearSRGBToLinearRec2020Matrix = mat3(
 	vec3(0.3293, 0.9195, 0.0880),
 	vec3(0.0433, 0.0113, 0.8956)
 );
+
+const vec3 LinearRec2020LuminanceWeights = vec3(0.2627002, 0.6779981, 0.0593017);
 
 #endif
