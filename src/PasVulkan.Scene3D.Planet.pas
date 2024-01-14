9393088c803786e1ff31bba3eda9611f994feb34
Benjamin Rosseaux <benjamin@rosseaux.com>
2024-01-14 06:56:38 +0100

    More work

--- src/PasVulkan.Scene3D.Planet.pas
+++ src/PasVulkan.Scene3D.Planet.pas
@@ -133,8 +133,8 @@ type TpvScene3DPlanets=class;
             PMaterial=^TMaterial;
             TMaterials=array[0..15] of TMaterial;
             PMaterials=^TMaterials;
-       const SourcePrimitiveMode:TpvScene3DPlanet.TSourcePrimitiveMode=TpvScene3DPlanet.TSourcePrimitiveMode.VisualMeshTriangles;
-             Direct:Boolean=true;
+       const SourcePrimitiveMode:TpvScene3DPlanet.TSourcePrimitiveMode=TpvScene3DPlanet.TSourcePrimitiveMode.OctasphereQuads;
+             Direct:Boolean=false;
        type TMeshVertex=record
              PositionAbsoluteHeight:TpvVector4;
              NormalRelativeHeight:TpvVector4;
