fbf15fc059e7c6342cda66916c9aae6206c50048
Benjamin Rosseaux <benjamin@rosseaux.com>
2024-01-29 03:33:23 +0100

    More work

--- src/assets/shaders/scene3d/projectsphere.glsl
+++ src/assets/shaders/scene3d/projectsphere.glsl
@@ -146,7 +146,7 @@ bool projectSphere(vec3 center, const in float radius, const in float zNear, con
   }else{
 
     center.z = -max(zNear, -center.z); // clamp center to near plane
-//  center.z = -max(zNear, (-center.z) - radius); // move and clamp center to near plane
+//  center.z = -max(zNear, (-center.z) - radius); // move towards the near plane and clamp center to near plane when necessary
 
     vec3 screenMin = (projectionMatrix * vec4(center - vec3(radius, radius, 0.0), 1.0)).xyw;
     screenMin.xy /= screenMin.z;
