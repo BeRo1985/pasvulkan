21e7327bdea68a05b43440302d6c28a1b5ca13ce
Benjamin Rosseaux <benjamin@rosseaux.com>
2024-03-14 13:09:30 +0100

    packscene3dshaders tool

---
+++ src/packscene3dshaders.dpr
@@ -0,0 +1,179 @@
+program packscene3dshaders;
+{$ifdef fpc}
+ {$mode delphi}
+ {$ifdef cpui386}
+  {$define cpu386}
+ {$endif}
+ {$ifdef cpu386}
+  {$asmmode intel}
+ {$endif}
+ {$ifdef cpuamd64}
+  {$asmmode intel}
+ {$endif}
+ {$ifdef fpc_little_endian}
+  {$define little_endian}
+ {$else}
+  {$ifdef fpc_big_endian}
+   {$define big_endian}
+  {$endif}
+ {$endif}
+ {$ifdef fpc_has_internal_sar}
+  {$define HasSAR}
+ {$endif}
+ {-$pic off}
+ {$define CAN_INLINE}
+ {$ifdef FPC_HAS_TYPE_EXTENDED}
+  {$define HAS_TYPE_EXTENDED}
+ {$else}
+  {$undef HAS_TYPE_EXTENDED}
+ {$endif}
+ {$ifdef FPC_HAS_TYPE_DOUBLE}
+  {$define HAS_TYPE_DOUBLE}
+ {$else}
+  {$undef HAS_TYPE_DOUBLE}
+ {$endif}
+ {$ifdef FPC_HAS_TYPE_SINGLE}
+  {$define HAS_TYPE_SINGLE}
+ {$else}
+  {$undef HAS_TYPE_SINGLE}
+ {$endif}
+{$else}
+ {$realcompatibility off}
+ {$localsymbols on}
+ {$define little_endian}
+ {$ifndef cpu64}
+  {$define cpu32}
+ {$endif}
+ {$define delphi} 
+ {$undef HasSAR}
+ {$define UseDIV}
+ {$define HAS_TYPE_EXTENDED}
+ {$define HAS_TYPE_DOUBLE}
+ {$define HAS_TYPE_SINGLE}
+{$endif}
+{$ifdef cpu386}
+ {$define cpux86}
+{$endif}
+{$ifdef cpuamd64}
+ {$define cpux86}
+{$endif}
+{$ifdef Win32}
+ {$define Windows}
+{$endif}
+{$ifdef Win64}
+ {$define Windows}
+{$endif}
+{$ifdef WinCE}
+ {$define Windows}
+{$endif}
+{$ifdef Windows}
+ {$define Win}
+{$endif}
+{$ifdef sdl20}
+ {$define sdl}
+{$endif}
+{$rangechecks off}
+{$extendedsyntax on}
+{$writeableconst on}
+{$hints off}
+{$booleval off}
+{$typedaddress off}
+{$stackframes off}
+{$varstringchecks on}
+{$typeinfo on}
+{$overflowchecks off}
+{$longstrings on}
+{$openstrings on}
+{$ifndef HAS_TYPE_DOUBLE}
+ {$error No double floating point precision}
+{$endif}
+{$ifdef fpc}
+ {$define CAN_INLINE}
+{$else}
+ {$undef CAN_INLINE}
+ {$ifdef ver180}
+  {$define CAN_INLINE}
+ {$else}
+  {$ifdef conditionalexpressions}
+   {$if compilerversion>=18}
+    {$define CAN_INLINE}
+   {$ifend}
+  {$endif}
+ {$endif}
+{$endif}
+{$ifdef windows}
+ {$apptype console}
+{$endif}
+{$undef UNICODE}
+
+uses SysUtils,
+     Classes,
+     PasMP in '../externals/pasmp/src/PasMP.pas',
+     PUCU in '../externals/pucu/src/PUCU.pas',
+     PasDblStrUtils in '../externals/pasdblstrutils/src/PasDblStrUtils.pas',
+     PasJSON in '../externals/pasjson/src/PasJSON.pas',
+     PasVulkan.Types,
+     PasVulkan.Math,
+     PasVulkan.Collections,
+     PasVulkan.Compression.LZMA,
+     PasVulkan.Archive.SPK;
+
+var OutputFileName,InputFileListFileName,FileName:String;
+    InputFileList:TStringList;
+    FileIndex:TpvSizeInt;
+    Archive:TpvArchiveSPK;
+    TemporaryUncompressedStream,TemporaryCompressedStream:TMemoryStream;
+begin
+
+ OutputFileName:=ParamStr(1);
+ InputFileListFileName:=ParamStr(2);
+
+ InputFileList:=TStringList.Create;
+ try
+
+  InputFileList.LoadFromFile(InputFileListFileName);
+
+  Archive:=TpvArchiveSPK.Create;
+  try
+
+   for FileIndex:=0 to InputFileList.Count-1 do begin
+    FileName:=InputFileList[FileIndex];
+    WriteLn('Adding "',FileName,'" . . .');
+    Archive.AddFile(LowerCase(FileName),FileName);
+   end;
+
+   TemporaryUncompressedStream:=TMemoryStream.Create;
+   try
+    
+    WriteLn('Saving . . .');
+    Archive.SaveToStream(TemporaryUncompressedStream);
+
+    TemporaryCompressedStream:=TMemoryStream.Create;
+    try
+     
+     WriteLn('Compressing . . .');
+     LZMACompressStream(TemporaryUncompressedStream,TemporaryCompressedStream,9);
+
+     WriteLn('Saving to "',OutputFileName,'" . . .');
+     TemporaryCompressedStream.SaveToFile(OutputFileName);
+
+    finally
+     FreeAndNil(TemporaryCompressedStream);
+    end;
+
+   finally
+    FreeAndNil(TemporaryUncompressedStream);
+   end; 
+
+  finally
+   FreeAndNil(Archive);
+  end;  
+  
+ finally
+  InputFileList.Free;
+ end;
+
+ WriteLn('Done.');
+
+end.     
+     
