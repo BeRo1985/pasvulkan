444328f85d09c30410c0f5c3f95ac01ec4536c09
Benjamin Rosseaux <benjamin@rosseaux.com>
2024-04-16 04:51:50 +0200

    More work

--- src/PasVulkan.Scene3D.pas
+++ src/PasVulkan.Scene3D.pas
@@ -20762,7 +20762,7 @@ begin
                      (fVulkanDevice.PhysicalDevice.MeshShaderFeaturesEXT.taskShader<>VK_FALSE){and
                      (fVulkanDevice.PhysicalDevice.MeshShaderFeaturesEXT.multiviewMeshShader<>VK_FALSE)};
 
- fHardwareRaytracingSupport:=false and
+ fHardwareRaytracingSupport:=//false and
                              aUseBufferDeviceAddress and
                              (fVulkanDevice.RayTracingPipelineFeaturesKHR.rayTracingPipeline<>VK_FALSE) and
                              (fVulkanDevice.RayQueryFeaturesKHR.rayQuery<>VK_FALSE);
