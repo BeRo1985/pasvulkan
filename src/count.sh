0dc17e2fe33921853c8ba90ab95e8a8c131fc9b5
Benjamin Rosseaux <benjamin@rosseaux.com>
2024-05-23 04:17:11 +0200

    More work

--- src/count.sh
+++ src/count.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-wc -l *.pas *.inc *.dpr ../externals/kraft/src/kraft.pas ../externals/kraft/src/KraftArcadeCarPhysics.pas ../externals/pasdblstrutils/src/PasDblStrUtils.pas ../externals/pasgltf/src/PasGLTF.pas ../externals/pasjson/src/PasJSON.pas ../externals/pasmp/src/PasMP.pas ../externals/pucu/src/PUCU.pas ../externals/rnl/src/RNL.pas
+wc -l *.pas *.inc *.dpr ../externals/kraft/src/kraft.pas \
+                        ../externals/kraft/src/KraftArcadeCarPhysics.pas \
+                        ../externals/kraft/src/KraftRayCastVehicle.pas \
+                        ../externals/pasdblstrutils/src/PasDblStrUtils.pas \
+                        ../externals/pasgltf/src/PasGLTF.pas \
+                        ../externals/pasjson/src/PasJSON.pas \
+                        ../externals/pasmp/src/PasMP.pas \
+                        ../externals/pucu/src/PUCU.pas \
+                        ../externals/rnl/src/RNL.pas
