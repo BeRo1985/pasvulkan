cec95d0d16754edff47e541e03dd8229d32687ba
Benjamin Rosseaux <benjamin@rosseaux.com>
2024-05-25 16:53:58 +0200

    More work

--- src/count.sh
+++ src/count.sh
@@ -8,4 +8,17 @@ wc -l *.pas *.inc *.dpr ../externals/kraft/src/kraft.pas \
                         ../externals/pasmp/src/PasMP.pas \
                         ../externals/pucu/src/PUCU.pas \
                         ../externals/poca/src/POCA.pas \
-                        ../externals/rnl/src/RNL.pas
+                        ../externals/rnl/src/RNL.pas \
+                        ./assets/shaders/canvas/*.glsl \
+                        ./assets/shaders/canvas/*.frag \
+                        ./assets/shaders/canvas/*.vert \
+                        ./assets/shaders/scene3d/*.glsl \
+                        ./assets/shaders/scene3d/*.frag \
+                        ./assets/shaders/scene3d/*.vert \
+                        ./assets/shaders/scene3d/*.geom \
+                        ./assets/shaders/scene3d/*.tesc \
+                        ./assets/shaders/scene3d/*.tese \
+                        ./assets/shaders/scene3d/*.comp \
+                        ./assets/shaders/scene3d/*.cpp \
+                        ./assets/shaders/scene3d/*.c \
+                        ./assets/shaders/scene3d/*.poca
