bf8748a4b22de65526dac1409e8fd32c8453778c
Benjamin Rosseaux <benjamin@rosseaux.com>
2024-05-28 15:52:31 +0200

    More work

--- src/assets/shaders/scene3d/tangentspace.glsl
+++ src/assets/shaders/scene3d/tangentspace.glsl
@@ -30,7 +30,7 @@
 
 uint encodeTangentSpaceAsRGB10A2SNorm(mat3 tbn){
 
-  // Normalized tangent space vectors,just for the sake of clarity and to be sure
+  // Normalize tangent space vectors, just for the sake of clarity and for to be sure
   tbn[0] = normalize(tbn[0]);
   tbn[1] = normalize(tbn[1]);
   tbn[2] = normalize(tbn[2]);
