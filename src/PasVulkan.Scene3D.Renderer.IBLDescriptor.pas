0654f213cad3e20c86ba0caae6c49174dd5d0676
Benjamin Rosseaux <benjamin@rosseaux.com>
2024-07-23 07:16:56 +0200

    More work

---
+++ src/PasVulkan.Scene3D.Renderer.IBLDescriptor.pas
@@ -0,0 +1,166 @@
+(******************************************************************************
+ *                                 PasVulkan                                  *
+ ******************************************************************************
+ *                       Version see PasVulkan.Framework.pas                  *
+ ******************************************************************************
+ *                                zlib license                                *
+ *============================================================================*
+ *                                                                            *
+ * Copyright (C) 2016-2024, Benjamin Rosseaux (benjamin@rosseaux.de)          *
+ *                                                                            *
+ * This software is provided 'as-is', without any express or implied          *
+ * warranty. In no event will the authors be held liable for any damages      *
+ * arising from the use of this software.                                     *
+ *                                                                            *
+ * Permission is granted to anyone to use this software for any purpose,      *
+ * including commercial applications, and to alter it and redistribute it     *
+ * freely, subject to the following restrictions:                             *
+ *                                                                            *
+ * 1. The origin of this software must not be misrepresented; you must not    *
+ *    claim that you wrote the original software. If you use this software    *
+ *    in a product, an acknowledgement in the product documentation would be  *
+ *    appreciated but is not required.                                        *
+ * 2. Altered source versions must be plainly marked as such, and must not be *
+ *    misrepresented as being the original software.                          *
+ * 3. This notice may not be removed or altered from any source distribution. *
+ *                                                                            *
+ ******************************************************************************
+ *                  General guidelines for code contributors                  *
+ *============================================================================*
+ *                                                                            *
+ * 1. Make sure you are legally allowed to make a contribution under the zlib *
+ *    license.                                                                *
+ * 2. The zlib license header goes at the top of each source file, with       *
+ *    appropriate copyright notice.                                           *
+ * 3. This PasVulkan wrapper may be used only with the PasVulkan-own Vulkan   *
+ *    Pascal header.                                                          *
+ * 4. After a pull request, check the status of your pull request on          *
+      http://github.com/BeRo1985/pasvulkan                                    *
+ * 5. Write code which's compatible with Delphi >= 2009 and FreePascal >=     *
+ *    3.1.1                                                                   *
+ * 6. Don't use Delphi-only, FreePascal-only or Lazarus-only libraries/units, *
+ *    but if needed, make it out-ifdef-able.                                  *
+ * 7. No use of third-party libraries/units as possible, but if needed, make  *
+ *    it out-ifdef-able.                                                      *
+ * 8. Try to use const when possible.                                         *
+ * 9. Make sure to comment out writeln, used while debugging.                 *
+ * 10. Make sure the code compiles on 32-bit and 64-bit platforms (x86-32,    *
+ *     x86-64, ARM, ARM64, etc.).                                             *
+ * 11. Make sure the code runs on all platforms with Vulkan support           *
+ *                                                                            *
+ ******************************************************************************)
+unit PasVulkan.Scene3D.Renderer.IBLDescriptor;
+{$i PasVulkan.inc}
+{$ifndef fpc}
+ {$ifdef conditionalexpressions}
+  {$if CompilerVersion>=24.0}
+   {$legacyifend on}
+  {$ifend}
+ {$endif}
+{$endif}
+
+interface
+
+uses SysUtils,
+     Classes,
+     Math,
+     Vulkan,
+     PasVulkan.Types,
+     PasVulkan.Framework;
+
+type { TpvScene3DRendererIBLDescriptor }     
+     TpvScene3DRendererIBLDescriptor=class
+      private
+       fVulkanDevice:TpvVulkanDevice;
+       fDescriptorSet:TpvVulkanDescriptorSet;
+       fBinding:TpvVulkanSizeInt;
+       fGGXImageView:TpvVulkanImageView;
+       fCharlieImageView:TpvVulkanImageView;
+       fLambertianImageView:TpvVulkanImageView;
+       fGGXImageDescriptor:TpvVulkanImageDescriptor;
+       fCharlieImageDescriptor:TpvVulkanImageDescriptor;
+       fLambertianImageDescriptor:TpvVulkanImageDescriptor;
+       fPointerToGGXImageDescriptor:PpvVulkanImageDescriptor;
+       fPointerToCharlieImageDescriptor:PpvVulkanImageDescriptor;
+       fPointerToLambertianImageDescriptor:PpvVulkanImageDescriptor;
+       fDirty:Boolean;
+       procedure SetGGXImageView(const aGGXImageView:TpvVulkanImageView);
+       procedure SetCharlieImageView(const aCharlieImageView:TpvVulkanImageView);
+       procedure SetLambertianImageView(const aLambertianImageView:TpvVulkanImageView);
+      public
+       constructor Create(const aVulkanDevice:TpvVulkanDevice;const aDescriptorSet:TpvVulkanDescriptorSet;const aBinding:TpvVulkanSizeInt); 
+       destructor Destroy; override;
+       procedure Update(const aInstant:Boolean=false);
+      public
+       property VulkanDevice:TpvVulkanDevice read fVulkanDevice;
+       property DescriptorSet:TpvVulkanDescriptorSet read fDescriptorSet;
+       property Binding:TpvVulkanSizeInt read fBinding;
+       property GGXImageView:TpvVulkanImageView read fGGXImageView write SetGGXImageView;
+       property CharlieImageView:TpvVulkanImageView read fCharlieImageView write SetCharlieImageView;
+       property LambertianImageView:TpvVulkanImageView read fLambertianImageView write SetLambertianImageView; 
+       property GGXImageDescriptor:PpvVulkanImageDescriptor read fPointerToGGXImageDescriptor;
+       property CharlieImageDescriptor:PpvVulkanImageDescriptor read fPointerToCharlieImageDescriptor;
+       property LambertianImageDescriptor:PpvVulkanImageDescriptor read fPointerToLambertianImageDescriptor;
+     end; 
+
+implementation
+
+constructor TpvScene3DRendererIBLDescriptor.Create(const aVulkanDevice:TpvVulkanDevice;const aDescriptorSet:TpvVulkanDescriptorSet;const aBinding:TpvVulkanSizeInt);
+begin
+ inherited Create;
+ fVulkanDevice:=aVulkanDevice;
+ fDescriptorSet:=aDescriptorSet;
+ fBinding:=aBinding;
+ fGGXImageView:=VK_NULL_HANDLE;
+ fCharlieImageView:=VK_NULL_HANDLE;
+ fLambertianImageView:=VK_NULL_HANDLE;
+ FillChar(fGGXImageDescriptor,SizeOf(TpvVulkanImageDescriptor),#0);
+ FillChar(fCharlieImageDescriptor,SizeOf(TpvVulkanImageDescriptor),#0);
+ FillChar(fLambertianImageDescriptor,SizeOf(TpvVulkanImageDescriptor),#0);
+ fPointerToGGXImageDescriptor:=@fGGXImageDescriptor;
+ fPointerToCharlieImageDescriptor:=@fCharlieImageDescriptor;
+ fPointerToLambertianImageDescriptor:=@fLambertianImageDescriptor;
+ fDirty:=true;
+end;
+
+destructor TpvScene3DRendererIBLDescriptor.Destroy;
+begin
+ inherited Destroy;
+end;
+
+procedure TpvScene3DRendererIBLDescriptor.SetGGXImageView(const aGGXImageView:TpvVulkanImageView);
+begin
+ if fGGXImageView<>aGGXImageView then begin
+  fGGXImageView:=aGGXImageView;
+  fGGXImageDescriptor.ImageView:=fGGXImageView;
+  fDirty:=true;
+ end;
+end;
+
+procedure TpvScene3DRendererIBLDescriptor.SetCharlieImageView(const aCharlieImageView:TpvVulkanImageView);
+begin
+ if fCharlieImageView<>aCharlieImageView then begin
+  fCharlieImageView:=aCharlieImageView;
+  fCharlieImageDescriptor.ImageView:=fCharlieImageView;
+  fDirty:=true;
+ end;
+end;
+
+procedure TpvScene3DRendererIBLDescriptor.SetLambertianImageView(const aLambertianImageView:TpvVulkanImageView);
+begin
+ if fLambertianImageView<>aLambertianImageView then begin
+  fLambertianImageView:=aLambertianImageView;
+  fLambertianImageDescriptor.ImageView:=fLambertianImageView;
+  fDirty:=true;
+ end;
+end;
+
+procedure TpvScene3DRendererIBLDescriptor.Update(const aInstant:Boolean=false);
+begin
+ if fDirty then begin
+  fDirty:=false;
+  fDescriptorSet.WriteToDescriptorSet(0,fBinding,1,VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,false,@fGGXImageDescriptor);
+ end;
+end;
+
+end.
