0596f39c6aae3e01a1b9898c73a320d0e1811049
Benjamin Rosseaux <benjamin@rosseaux.com>
2024-12-02 06:31:44 +0100

    More work

--- src/PasVulkan.Scene3D.Renderer.Passes.CanvasRenderPass.pas
+++ src/PasVulkan.Scene3D.Renderer.Passes.CanvasRenderPass.pas
@@ -384,10 +384,10 @@ begin
     // Solid primitives uses additive blending with premultiplied alpha
     VulkanGraphicsPipeline.ColorBlendState.AddColorBlendAttachmentState(true,
                                                                         VK_BLEND_FACTOR_ONE,
-                                                                        VK_BLEND_FACTOR_ONE,
+                                                                        VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
                                                                         VK_BLEND_OP_ADD,
                                                                         VK_BLEND_FACTOR_ONE,
-                                                                        VK_BLEND_FACTOR_ONE,
+                                                                        VK_BLEND_FACTOR_ZERO,
                                                                         VK_BLEND_OP_ADD,
                                                                         TVkColorComponentFlags(VK_COLOR_COMPONENT_R_BIT) or
                                                                         TVkColorComponentFlags(VK_COLOR_COMPONENT_G_BIT) or
