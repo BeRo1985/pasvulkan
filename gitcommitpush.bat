baae4ac5c2dd9263e5017388aedd2c7fdd158c7d
Benjamin Rosseaux <benjamin@rosseaux.com>
2025-04-03 12:22:38 +0200

    Updated gitcommitpush.bat

--- gitcommitpush.bat
+++ gitcommitpush.bat
@@ -1,4 +1,8 @@
 @echo off
-call git commit -am "More work"
-call git push 
+set /p commit_message="Enter commit message (or leave blank for default): "
+if "%commit_message%"=="" (
+    set commit_message=More work
+)
+git commit -am "%commit_message%"
+git push
 rem --set-upstream origin master
