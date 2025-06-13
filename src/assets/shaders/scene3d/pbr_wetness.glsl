7483ae28d5d3752c39deab1d18a1980a045f04f3
Benjamin Rosseaux <benjamin@rosseaux.com>
2025-06-13 17:02:33 +0200

    More work

--- src/assets/shaders/scene3d/pbr_wetness.glsl
+++ src/assets/shaders/scene3d/pbr_wetness.glsl
@@ -24,24 +24,81 @@ void applyPBRWetness(
   // Not optimal yet, just the foundation for wetness application in PBR as first version.
   if(wetness.x > 0.0){
 
-    // Apply wetness to albedo
-    //albedo = mix(albedo, albedo * vec3(0.2, 0.8, 1.0), wetness.x * 0.1); // Blend with blue color for wetness
+    // Calculate the scaled position and its derivatives for triplanar mapping
+    vec3 scaledPosition = position * 0.25; // Scale position for triplanar mapping
+    vec3 dpdx = dFdx(scaledPosition), dpdy = dFdy(scaledPosition); // Calculate derivatives for texture gradients
 
-    // Apply wetness to normal
-    //normal = normalize(mix(normal, wetness.yzw, wetness.x * 0.1)); // Blend normal with the provided normal based on wetness factor
+    // Calculate the tangent space basis from the wetness normal
+    // wetness.yzw is the normal from the ground, we need to calculate the tangent and bitangent vectors
+    // to create a tangent space basis for triplanar mapping
+    vec3 n = wetness.yzw, t = n.yzx - n.zxy, b = normalize(cross(n, t = normalize(t - dot(t, n))));
+    mat3 tbn = mat3x3(t, b, n); 
 
-    // Apply wetness to metallic and roughness
-//  metallic = mix(metallic, 0.0, wetness.x); // Decrease metallic with wetness
-  //  roughness = mix(roughness, roughness * 0.1, wetness.x); // Decrease roughness with wetness
-//  occlusion = mix(occlusion, 1.0, wetness.x); // Increase occlusion with wetness
+#define USE_PBR_WETNESS_BIPLANAR 1
+#if USE_PBR_WETNESS_BIPLANAR
+    vec3 absNormal = abs(n);
+    ivec3 majorAxis = ((absNormal.x > absNormal.y) && (absNormal.x > absNormal.z)) ? ivec3(0, 1, 2) : ((absNormal.y > absNormal.z) ? ivec3(1, 2, 0) : ivec3(2, 0, 1));
+    ivec3 minorAxis = ((absNormal.x < absNormal.y) && (absNormal.x < absNormal.z)) ? ivec3(0, 1, 2) : ((absNormal.y < absNormal.z) ? ivec3(1, 2, 0) : ivec3(2, 0, 1));
+    ivec3 medianAxis = (ivec3(3) - minorAxis) - majorAxis;
+    vec2 biplanarWeights = pow(clamp((vec2(normal[majorAxis.x], normal[medianAxis.x]) - vec2(0.5773)) / vec2(1.0 - 0.5773), vec2(0.0), vec2(1.0)), vec2(8.0 * 0.125));
+    float totalWeight = biplanarWeights.x + biplanarWeights.y;
+    if(totalWeight > 0.0){
+      biplanarWeights /= totalWeight; // Normalize the weights
+    } else {
+      biplanarWeights = vec2(1.0 / 2.0); // Default weights if total weight is zero
+    }
+    #define PBR_WETNESS_FETCH_TEXTURE(source, pos) \
+      ( \
+        (textureGrad(source, vec2(pos[majorAxis.y], pos[majorAxis.z]), vec2(dpdx[majorAxis.y], dpdx[majorAxis.z]), vec2(dpdy[majorAxis.y], dpdy[majorAxis.z])) * biplanarWeights.x) + \
+        (textureGrad(source, vec2(pos[medianAxis.y], pos[medianAxis.z]), vec2(dpdx[medianAxis.y], dpdx[medianAxis.z]), vec2(dpdy[medianAxis.y],dpdy[medianAxis.z])) * biplanarWeights.y) \
+      ) 
+    #define PBR_WETNESS_FETCH_TEXTURE_CHANNEL(source, pos, c) \
+      dot( \
+        vec2( \
+          textureGrad(source, vec2(pos[majorAxis.y], pos[majorAxis.z]), vec2(dpdx[majorAxis.y], dpdx[majorAxis.z]), vec2(dpdy[majorAxis.y], dpdy[majorAxis.z]))[c], \
+          textureGrad(source, vec2(pos[medianAxis.y], pos[medianAxis.z]), vec2(dpdx[medianAxis.y], dpdx[medianAxis.z]), vec2(dpdy[medianAxis.y],dpdy[medianAxis.z]))[c] \
+        ), \
+        biplanarWeights \
+      )
+#else
+    // Calculate the triplanar weights based on the tangent space basis
+    vec3 triplanarWeights = abs(tbn[2]);
+    {
+      // pow(triplanarWeights, vec3(8.0) => 3x sq 
+      triplanarWeights *= triplanarWeights; 
+      triplanarWeights *= triplanarWeights; 
+      triplanarWeights *= triplanarWeights; 
+    }
+    float totalWeight = dot(triplanarWeights, vec3(1.0));
+    if(totalWeight > 0.0){
+      triplanarWeights /= totalWeight; // Normalize the weights
+    } else {
+      triplanarWeights = vec3(1.0 / 3.0); // Default weights if total weight is zero
+    }
+    #define PBR_WETNESS_FETCH_TEXTURE(source, pos) \
+      ( \
+        (textureGrad(source, pos.yz, dpdx.yz, dpdy.yz).xyz * triplanarWeights.x) + \
+        (textureGrad(source, pos.zx, dpdx.zx, dpdy.zx).xyz * triplanarWeights.y) + \
+        (textureGrad(source, pos.xy, dpdx.xy, dpdy.xy).xyz * triplanarWeights.z) \ 
+      )
+    #define PBR_WETNESS_FETCH_TEXTURE_CHANNEL(source, pos, c) \
+      dot( \
+        vec3( \
+          textureGrad(source, pos.yz, dpdx.yz, dpdy.yz)[c], \
+          textureGrad(source, pos.zx, dpdx.zx, dpdy.zx)[c], \
+          textureGrad(source, pos.xy, dpdx.xy, dpdy.xy)[c] \
+        ), \
+        triplanarWeights \
+      )
+#endif
 
-    float normalDotGround = dot(tangentSpaceBasis[2], wetness.yzw); // Dot product with ground normal to determine wetness effect on normal
+    float normalDotGround = dot(tangentSpaceBasis[2], tbn[2]); // Dot product with ground normal to determine wetness effect on normal
 
-    // Triplanar mapping for puddles effect
+    // UV mapping for puddles effect
     vec2 puddlesUV = vec2(
-      dot(tangentSpaceBasis[0], position), 
-      dot(tangentSpaceBasis[1], position)
-    ) * 0.25;
+      dot(tbn[0], scaledPosition), 
+      dot(tbn[1], scaledPosition)
+    );
 
     // Calculate fractional time for puddles effect
     vec2 fracPuddleTimes = fract(vec2(vec2(rainTime * rainSpeed) + vec2(0.0, 0.5))); // Offset for staggered puddles effect
@@ -68,29 +125,18 @@ void applyPBRWetness(
       clamp((normalDotGround - 0.2) * 5.0, 0.0, 1.0) * // Puddles only on top 
       wetness.x; // Apply wetness factor to puddles, no rain, no puddles 
 
-    // Calcularte the UV coordinates for rain streaks
-    vec2 streaksUV = vec2(
-      dot(tangentSpaceBasis[1], position),
-      dot(tangentSpaceBasis[2], position)
-    ) * 0.25;
-
     // Calculate the rain streaks effect
     float underRoof = smoothstep(-0.3, 0.0, normalDotGround);
-    float streaks = texture(rainTexture, streaksUV).y * // Get rain streaks from the texture   
+    float streaks = PBR_WETNESS_FETCH_TEXTURE_CHANNEL(rainTexture, scaledPosition, 1) * // Get rain streaks from the texture
                     (1.0 - clamp((normalDotGround - 0.05) * 20.0, 0.0, 1.0)) * // Streaks not on top, only on sides 
                     wetness.x; // Apply wetness factor to streaks, no rain, no streaks
+    vec3 offsetedPosition = scaledPosition + (tbn[2] * (rainTime * rainSpeed * 0.2));
     streaks = smoothstep(
       0.0,
       0.1,
       streaks * 
       clamp(
-        texture(
-          rainTexture,
-          vec2(
-            streaksUV.x,
-            fract(streaksUV.y + (rainTime * rainSpeed * 0.2))
-          )
-        ).z - 0.5,
+        PBR_WETNESS_FETCH_TEXTURE_CHANNEL(rainTexture, offsetedPosition, 2) - 0.5, // Adjust the threshold for rain streaks   
         0.0,
         1.0
       )
@@ -105,7 +151,7 @@ void applyPBRWetness(
           puddles.xyz,
           puddles.w // Apply puddles effect
         ),
-        fma(texture(rainStreakNormalTexture, streaksUV).xyz, vec3(2.0), vec3(-1.0)), // Normal from rain texture
+        fma(PBR_WETNESS_FETCH_TEXTURE(rainStreakNormalTexture, scaledPosition).xyz, vec3(2.0), vec3(-1.0)),
         streaks // Apply streaks effect
       );           
     normal = vec4(rainNormal, 1.0); // Set the normal vector for normal mapping
