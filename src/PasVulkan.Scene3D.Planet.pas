0d6950f59e0c9a627289a051c3e7f732d09d4229
Benjamin Rosseaux <benjamin@rosseaux.com>
2025-08-25 09:25:31 +0200

    More work

--- src/PasVulkan.Scene3D.Planet.pas
+++ src/PasVulkan.Scene3D.Planet.pas
@@ -27280,14 +27280,14 @@ begin
       fPrecipitationAtmosphereMapCombination.Execute(aCommandBuffer);
       
       // Add memory barrier to ensure PrecipitationAtmosphereMapBuffer writes are visible to atmosphere rendering
-      BufferBarrier:=TVkBufferMemoryBarrier.Create(VK_ACCESS_SHADER_WRITE_BIT,
-                                                   VK_ACCESS_SHADER_READ_BIT,
+      BufferBarrier:=TVkBufferMemoryBarrier.Create(TVkAccessFlags(VK_ACCESS_SHADER_WRITE_BIT),
+                                                   TVkAccessFlags(VK_ACCESS_SHADER_READ_BIT),
                                                    VK_QUEUE_FAMILY_IGNORED,
                                                    VK_QUEUE_FAMILY_IGNORED,
                                                    fData.fPrecipitationAtmosphereMapBuffer.Handle,
                                                    0,
                                                    fData.fPrecipitationAtmosphereMapBuffer.Size);
-                                                   
+
       aCommandBuffer.CmdPipelineBarrier(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT,
                                         VK_PIPELINE_STAGE_VERTEX_SHADER_BIT or VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
                                         0,
