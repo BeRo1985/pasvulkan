0d744738a5df04e5d45affee2fa827dabbe5a55c
Benjamin Rosseaux <benjamin@rosseaux.com>
2026-01-10 10:50:55 +0100

    More work

--- src/assets/shaders/scene3d/mesh_pushconstants.glsl
+++ src/assets/shaders/scene3d/mesh_pushconstants.glsl
@@ -8,6 +8,7 @@ layout (push_constant) uniform PushConstants {
   uint frameIndex;
   vec4 jitter;
   uvec4 timeSecondsTimeFractionalSecondWidthHeight; // x = timeSeconds (uint), y = timeFractionalSecond (float), z = width, w = height
+  uint raytracingFlags;
 } pushConstants;
 
 #endif
