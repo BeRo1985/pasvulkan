8c37e7ce5743e020296d30072a893bb41fd9ca47
Benjamin Rosseaux <benjamin@rosseaux.com>
2026-04-09 19:56:44 +0200

    MeshShaderPushConstantStageFlags switch

--- src/PasVulkan.Scene3D.Renderer.Instance.pas
+++ src/PasVulkan.Scene3D.Renderer.Instance.pas
@@ -8071,10 +8071,16 @@ begin
    vkCmdDrawMeshTasksIndirectCountEXT:=Renderer.VulkanDevice.Commands.Commands.CmdDrawMeshTasksIndirectCountEXT;
    OutputBufferDeviceAddress:=fPerInFlightFrameGPUDrawIndexedIndirectCommandOutputBuffers[aInFlightFrameIndex].DeviceAddress;
    MeshStagePushConstants^.MeshDrawCommandsBDA:=OutputBufferDeviceAddress;
-   MeshShaderPushConstantStageFlags:=TVkShaderStageFlags(VK_SHADER_STAGE_VERTEX_BIT) or
-                                     TVkShaderStageFlags(VK_SHADER_STAGE_FRAGMENT_BIT) or
-                                     TVkShaderStageFlags(VK_SHADER_STAGE_TASK_BIT_EXT) or
-                                     TVkShaderStageFlags(VK_SHADER_STAGE_MESH_BIT_EXT);
+   if Renderer.UseMeshletExpand then begin
+    MeshShaderPushConstantStageFlags:=TVkShaderStageFlags(VK_SHADER_STAGE_VERTEX_BIT) or
+                                      TVkShaderStageFlags(VK_SHADER_STAGE_FRAGMENT_BIT) or
+                                      TVkShaderStageFlags(VK_SHADER_STAGE_MESH_BIT_EXT);
+   end else begin
+    MeshShaderPushConstantStageFlags:=TVkShaderStageFlags(VK_SHADER_STAGE_VERTEX_BIT) or
+                                      TVkShaderStageFlags(VK_SHADER_STAGE_FRAGMENT_BIT) or
+                                      TVkShaderStageFlags(VK_SHADER_STAGE_TASK_BIT_EXT) or
+                                      TVkShaderStageFlags(VK_SHADER_STAGE_MESH_BIT_EXT);
+   end;
    UseMeshShaderDraw:=true;
   end else begin
    vkCmdDrawMeshTasksIndirectCountEXT:=nil;
